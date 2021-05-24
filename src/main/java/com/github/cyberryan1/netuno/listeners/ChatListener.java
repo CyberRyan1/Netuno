4c8723bdc3204a626edcf11362508c95b0888a4a
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-05-24 18:23:13 -0500

    Added mute expire messages for the player muteed and for staff

--- src/main/java/com/github/cyberryan1/netuno/listeners/ChatListener.java
+++ src/main/java/com/github/cyberryan1/netuno/listeners/ChatListener.java
@@ -1,8 +1,12 @@
 package com.github.cyberryan1.netuno.listeners;
 
+import com.github.cyberryan1.netuno.utils.ConfigUtils;
 import com.github.cyberryan1.netuno.utils.Punishment;
 import com.github.cyberryan1.netuno.utils.Utils;
+import com.github.cyberryan1.netuno.utils.VaultUtils;
 import com.github.cyberryan1.netuno.utils.database.Database;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -15,11 +19,35 @@ public class ChatListener implements Listener {
 
     @EventHandler
     public void onPlayerChatEvent( AsyncPlayerChatEvent event ) {
+        ArrayList<Punishment> allPunishments = DATA.getPunishment( event.getPlayer().getUniqueId().toString() );
+        boolean hadActive = false;
+        for ( Punishment pun : allPunishments ) {
+            if ( pun.getActive() == true && pun.getType().equalsIgnoreCase( "mute" ) ) {
+                hadActive = true;
+                break;
+            }
+        }
+
         ArrayList<Punishment> punishments = DATA.getPunishment( event.getPlayer().getUniqueId().toString(), "mute", true );
         if ( punishments.size() >= 1 ) {
             event.setCancelled( true );
             Punishment first = punishments.get( 0 );
             Utils.sendDeniedMsg( event.getPlayer(), first );
+        }
+
+        else if ( hadActive ) {
+            if ( ConfigUtils.checkListNotEmpty( "mute.expire" ) ) {
+                Utils.sendAnyMsg( event.getPlayer(), ConfigUtils.getColoredStrFromList( "mute.expire" ) );
+            }
+
+            if ( ConfigUtils.checkListNotEmpty( "mute.expire-staff" ) ) {
+                String msg = ConfigUtils.getColoredStrFromList( "mute.expire-staff" );
+                for ( Player p : Bukkit.getOnlinePlayers() ) {
+                    if ( VaultUtils.hasPerms( p, ConfigUtils.getStr( "general.staff-perm" ) ) ) {
+                        Utils.sendAnyMsg( p, msg.replace( "[TARGET]", event.getPlayer().getName() ) );
+                    }
+                }
+            }
         }
     }
 }
