f434dfe96fcdf8930bfdb7107bd217a93668eeea
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-06-02 22:03:59 -0500

    Added unipban command

--- src/main/java/com/github/cyberryan1/netuno/Netuno.java
+++ src/main/java/com/github/cyberryan1/netuno/Netuno.java
@@ -8,7 +8,7 @@ import com.github.cyberryan1.netuno.utils.Utils;
 import com.github.cyberryan1.netuno.utils.VaultUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
-// TODO ipmute needs to alert all online alts and add notifications for all their alts (same with ipban, except no notifications)
+// TODO unipmute needs to alert all online alts
 // TODO add tab-completer
 // TODO add option to disable signs for muted players
 // TODO add sign-popups for staff
@@ -42,6 +42,7 @@ public final class Netuno extends JavaPlugin {
         this.getCommand( "ipmute" ).setExecutor( new IPMute() );
         this.getCommand( "unipmute" ).setExecutor( new UnIPMute() );
         this.getCommand( "ipban" ).setExecutor( new IPBan() );
+        this.getCommand( "unipban" ).setExecutor( new UnIPBan() );
 
         this.getServer().getPluginManager().registerEvents( new JoinListener(), this );
         this.getServer().getPluginManager().registerEvents( new ChatListener(), this );
---
+++ src/main/java/com/github/cyberryan1/netuno/commands/UnIPBan.java
@@ -0,0 +1,80 @@
+package com.github.cyberryan1.netuno.commands;
+
+import com.github.cyberryan1.netuno.utils.*;
+import com.github.cyberryan1.netuno.utils.database.Database;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+public class UnIPBan implements CommandExecutor {
+
+    private Database DATA = Utils.getDatabase();
+
+    @Override
+    public boolean onCommand( CommandSender sender, Command command, String label, String args[] ) {
+
+        if ( VaultUtils.hasPerms( sender, ConfigUtils.getStr( "unipban.perm" ) ) == false ) {
+            CommandErrors.sendInvalidPerms( sender );
+            return true;
+        }
+
+        if ( Utils.isOutOfBounds( args, 0 ) == false ) {
+
+            if ( Utils.isValidUsername( args[0] ) == false ) {
+                CommandErrors.sendPlayerNotFound( sender, args[0] );
+                return true;
+            }
+
+            OfflinePlayer target = Bukkit.getOfflinePlayer( args[0] );
+            if ( target != null ) {
+                ArrayList<IPPunishment> punishments = DATA.getIPPunishment( target.getUniqueId().toString(), "ipban", true );
+                if ( punishments.size() >= 1 ) {
+                    for ( IPPunishment pun : punishments ) {
+                        DATA.setIPPunishmentActive( pun.getID(), false );
+                    }
+
+                    IPPunishment pun = new IPPunishment();
+                    pun.setPlayerUUID( target.getUniqueId().toString() );
+                    pun.setReason( "" );
+                    pun.setLength( -1L );
+                    pun.setDate( Time.getCurrentTimestamp() );
+                    pun.setType( "UnIPBan" );
+                    pun.setActive( false );
+
+                    pun.setStaffUUID( "CONSOLE" );
+                    if ( sender instanceof Player ) {
+                        Player staff = ( Player ) sender;
+                        pun.setStaffUUID( staff.getUniqueId().toString() );
+                    }
+
+                    DATA.addPunishment( pun );
+
+                    Utils.doPublicPunBroadcast( pun );
+                    Utils.doStaffPunBroadcast( pun );
+
+                }
+
+                else {
+                    CommandErrors.sendNoPunishments( sender, target.getName(), "ipban" );
+                }
+
+            }
+
+            else {
+                CommandErrors.sendPlayerNotFound( sender, args[0] );
+            }
+
+        }
+
+        else {
+            CommandErrors.sendCommandUsage( sender, "unipban" );
+        }
+
+        return true;
+    }
+}
