910f30e15faa4b8737b5efadbc2235b523c98276
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-06-14 10:55:03 -0500

    Forgot to add the unban punishment to the database

--- src/main/java/com/github/cyberryan1/netuno/commands/Unban.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/Unban.java
@@ -48,6 +48,8 @@ public class Unban implements CommandExecutor {
                         unbanPun.setStaffUUID( staff.getUniqueId().toString() );
                     }
 
+                    DATA.addPunishment( unbanPun );
+
                     Utils.doPublicPunBroadcast( unbanPun );
                     Utils.doStaffPunBroadcast( unbanPun );
                 }
