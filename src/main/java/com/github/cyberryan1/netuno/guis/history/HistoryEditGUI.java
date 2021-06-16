d1fca63fa2348a2cc8cfb23c10944524d5ff5352
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-06-16 11:22:29 -0500

    Added onInventoryClose() event

--- src/main/java/com/github/cyberryan1/netuno/guis/history/HistoryEditGUI.java
+++ src/main/java/com/github/cyberryan1/netuno/guis/history/HistoryEditGUI.java
@@ -12,6 +12,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.inventory.Inventory;
@@ -203,6 +204,14 @@ public class HistoryEditGUI implements Listener {
         if ( event.getView().getTitle().equals( Utils.getColored( "&7Edit Punishment &6#" + targetPun.getID() ) ) == false ) { return; }
 
         event.setCancelled( true );
+    }
+
+    @EventHandler
+    public void onInventoryClose( InventoryCloseEvent event ) {
+        if ( STAFF_TARGETS.containsKey( event.getPlayer().getName() ) ) {
+            STAFF_TARGETS.remove( event.getPlayer().getName() );
+            STAFF_PUNS.remove( event.getPlayer().getName() );
+        }
     }
 
     @EventHandler
