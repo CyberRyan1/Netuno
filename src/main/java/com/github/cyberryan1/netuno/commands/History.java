6f4333ebcb2c91ecc827adaeea0c46c780f94abc
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-06-15 23:23:11 -0500

    Added /history edit (pun ID) command

--- src/main/java/com/github/cyberryan1/netuno/commands/History.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/History.java
@@ -1,5 +1,6 @@
 package com.github.cyberryan1.netuno.commands;
 
+import com.github.cyberryan1.netuno.guis.history.HistoryEditGUI;
 import com.github.cyberryan1.netuno.guis.history.HistoryListGUI;
 import com.github.cyberryan1.netuno.utils.*;
 import com.github.cyberryan1.netuno.utils.database.Database;
@@ -10,15 +11,15 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.UUID;
+
 public class History implements CommandExecutor {
 
     private final Database DATA = Utils.getDatabase();
 
     @Override
     // /history list (player)
-    // /history reason (pun ID) (new reason)
-    // /history time (pun ID) (new time)
-    // /history delete (pun ID)
+    // /history edit (pun ID)
     // /history reset (player)
     public boolean onCommand( CommandSender sender, Command command, String label, String args[] ) {
 
@@ -60,17 +61,49 @@ public class History implements CommandExecutor {
                 }
             }
 
-            else if ( args[0].equalsIgnoreCase( "reason" ) ) {
-                return false;
+            else if ( args[0].equalsIgnoreCase( "edit" ) ) {
+                if ( Utils.isOutOfBounds( args, 1 ) == false ) {
+
+                    if ( sender instanceof Player == false ) {
+                        CommandErrors.sendCanOnlyBeRanByPlayer( sender );
+                        return true;
+                    }
+
+                    int id = -1;
+                    try {
+                        id = Integer.parseInt( args[1] );
+                    } catch ( NumberFormatException e ) {
+                        CommandErrors.sendInvalidPunishmentID( sender, args[1] );
+                        return true;
+                    }
+
+                    if ( DATA.checkPunIDExists( id ) || DATA.checkIpPunIDExists( id ) ) {
+                        Punishment pun;
+                        if ( DATA.checkIpPunIDExists( id ) ) {
+                            pun = ( Punishment ) DATA.getIPPunishment( id );
+                        }
+                        else {
+                            pun = DATA.getPunishment( id );
+                        }
+
+                        OfflinePlayer target = Bukkit.getOfflinePlayer( UUID.fromString( pun.getPlayerUUID() ) );
+                        Player staff = ( Player ) sender;
+                        HistoryEditGUI editGUI = new HistoryEditGUI( target, staff, id );
+                        editGUI.openInventory( staff );
+                        Utils.getPlugin().getServer().getPluginManager().registerEvents( editGUI, Utils.getPlugin() );
+                    }
+
+                    else {
+                        CommandErrors.sendPunishmentIDNotFound( sender, id );
+                    }
+
+                }
+
+                else {
+                    CommandErrors.sendCommandUsage( sender, "history-edit" );
+                }
             }
 
-            else if ( args[0].equalsIgnoreCase( "time" ) ) {
-                return false;
-            }
-
-            else if ( args[0].equalsIgnoreCase( "delete" ) ) {
-                return false;
-            }
 
             else if ( args[0].equalsIgnoreCase( "reset" ) ) {
                 return false;
