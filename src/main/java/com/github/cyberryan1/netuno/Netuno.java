e9f4a87a14e52798a34948ccb2df557d22763a7a
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-06-23 14:15:50 -0500

    Removed 1x TODO

--- src/main/java/com/github/cyberryan1/netuno/Netuno.java
+++ src/main/java/com/github/cyberryan1/netuno/Netuno.java
@@ -9,7 +9,6 @@ import com.github.cyberryan1.netuno.utils.Utils;
 import com.github.cyberryan1.netuno.utils.VaultUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
-// TODO add alt-notifs
 // TODO add a notes system
 // TODO add a reports system
 // TODO add option to disable books for muted/ipmuted players
