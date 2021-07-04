53abc0fa982d114aa4adfb4fb061ff6519fcb618
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-07-04 00:04:35 -0500

    Added 1x TODO

--- src/main/java/com/github/cyberryan1/netuno/Netuno.java
+++ src/main/java/com/github/cyberryan1/netuno/Netuno.java
@@ -21,6 +21,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 // * TODO add a search by target in the staff reports GUI
 // * TODO add option to disable books for muted/ipmuted players
 
+// TODO use close() method in Database class
 // TODO add punish GUI
 // TODO add tab-completer
 // TODO add a way to convert vanilla bans to netuno bans
