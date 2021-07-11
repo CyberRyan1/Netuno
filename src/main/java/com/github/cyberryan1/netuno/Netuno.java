f1bd424a90bd73a6518798550377a1c38a1d6fef
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-07-11 17:09:24 -0500

    Completed 1x TODO

--- src/main/java/com/github/cyberryan1/netuno/Netuno.java
+++ src/main/java/com/github/cyberryan1/netuno/Netuno.java
@@ -18,7 +18,6 @@ import org.bukkit.plugin.java.JavaPlugin;
     - ? = unsure if doing */
 
 // * TODO add punish GUI
-// * TODO add a prevention system for multiple staff being in the same player's punish GUI (configurable)
 
 // TODO add tab-completer
 // TODO add option to disable books for muted/ipmuted players
