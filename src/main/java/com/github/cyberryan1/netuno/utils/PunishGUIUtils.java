7611363ad21f89ffc2add9465ce3c0a18b53932b
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-07-11 15:20:31 -0500

    Added standard getter methods

--- src/main/java/com/github/cyberryan1/netuno/utils/PunishGUIUtils.java
+++ src/main/java/com/github/cyberryan1/netuno/utils/PunishGUIUtils.java
@@ -5,14 +5,45 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 public class PunishGUIUtils {
 
-    private static PunishGUIManager punishGUIManager;
+    private static PunishGUIManager manager;
 
 
     public PunishGUIUtils( PunishGUIManager pgm ) {
-        punishGUIManager = pgm;
+        manager = pgm;
     }
 
-    public static FileConfiguration getConfig() { return punishGUIManager.getConfig(); }
+    public static FileConfiguration getConfig() { return manager.getConfig(); }
 
-    public static PunishGUIManager getPunishGUIManager() { return punishGUIManager; }
+    public static PunishGUIManager getPunishGUIManager() { return manager; }
+
+    public static boolean getBool( String path ) {
+        checkPath( path );
+        return manager.getConfig().getBoolean( path );
+    }
+
+    public static int getInt( String path ) {
+        checkPath( path );
+        return manager.getConfig().getInt( path );
+    }
+
+    public static double getDouble( String path ) {
+        checkPath( path );
+        return manager.getConfig().getDouble( path );
+    }
+
+    public static String getStr( String path ) {
+        checkPath( path );
+        return manager.getConfig().getString( path );
+    }
+
+    public static String getColoredStr( String path ) {
+        checkPath( path );
+        return manager.getConfig().getString( path );
+    }
+
+    private static void checkPath( String path ) {
+        if ( manager.getConfig().get( path ) == null ) {
+            Utils.logError( "Punish GUI path " + path + " was not found, please check your punishgui file!" );
+        }
+    }
 }
