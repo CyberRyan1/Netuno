08f4d1f53f5f7ea47a045e74a5d5e367e4bcdc74
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-07-12 19:28:48 -0500

    Created StaffPlayerPunishManager

---
+++ src/main/java/com/github/cyberryan1/netuno/managers/StaffPlayerPunishManager.java
@@ -0,0 +1,26 @@
+package com.github.cyberryan1.netuno.managers;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+public class StaffPlayerPunishManager {
+
+    private final static HashMap<Player, OfflinePlayer> punishing = new HashMap<>();
+
+    public Player getWhoPunishingTarget( OfflinePlayer target ) {
+        if ( punishing.containsValue( target ) == false ) { return null; }
+        for ( Player p : punishing.keySet() ) {
+            if ( punishing.get( p ).equals( target ) ) {
+                return p;
+            }
+        }
+
+        return null;
+    }
+
+    public OfflinePlayer getWhoStaffPunishing( Player staff ) {
+        return punishing.get( staff );
+    }
+}
