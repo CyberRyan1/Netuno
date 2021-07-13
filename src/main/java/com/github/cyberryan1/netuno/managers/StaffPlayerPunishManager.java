6b3d7428e15b5a3495fd976f773568adac0b9e85
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-07-12 19:32:07 -0500

    Made all methods static

--- src/main/java/com/github/cyberryan1/netuno/managers/StaffPlayerPunishManager.java
+++ src/main/java/com/github/cyberryan1/netuno/managers/StaffPlayerPunishManager.java
@@ -12,7 +12,7 @@ public class StaffPlayerPunishManager {
 
     private final static HashMap<Player, OfflinePlayer> punishing = new HashMap<>();
 
-    public Player getWhoPunishingTarget( OfflinePlayer target ) {
+    public static Player getWhoPunishingTarget( OfflinePlayer target ) {
         if ( punishing.containsValue( target ) == false ) { return null; }
         for ( Player p : punishing.keySet() ) {
             if ( punishing.get( p ).equals( target ) ) {
@@ -23,15 +23,15 @@ public class StaffPlayerPunishManager {
         return null;
     }
 
-    public OfflinePlayer getWhoStaffPunishing( Player staff ) {
+    public static OfflinePlayer getWhoStaffPunishing( Player staff ) {
         return punishing.get( staff );
     }
 
-    public void addStaffTarget( Player staff, OfflinePlayer target ) {
+    public static void addStaffTarget( Player staff, OfflinePlayer target ) {
         punishing.put( staff, target );
     }
 
-    public void removeStaff( Player staff ) {
+    public static void removeStaff( Player staff ) {
         punishing.remove( staff );
     }
 }
