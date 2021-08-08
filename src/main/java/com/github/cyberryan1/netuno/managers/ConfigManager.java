a3c5f73c97157a0baacb67e35a953a83a664f4cf
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-08-08 00:00:30 -0500

    Added updateConfig() method

--- src/main/java/com/github/cyberryan1/netuno/managers/ConfigManager.java
+++ src/main/java/com/github/cyberryan1/netuno/managers/ConfigManager.java
@@ -1,12 +1,16 @@
 package com.github.cyberryan1.netuno.managers;
 
 import com.github.cyberryan1.netuno.Netuno;
+import com.tchristofferson.configupdater.ConfigUpdater;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class ConfigManager {
 
@@ -61,5 +65,22 @@ public class ConfigManager {
         if ( configFile.exists() == false ) {
             plugin.saveResource( "config.yml", false );
         }
+    }
+
+    // Checks if any new keys, comments, etc, have been added via a plugin update
+    // And adds them to the current config (if needed)
+    // Note: must reload the config afterward for changes to come into effect
+    public void updateConfig() {
+        if ( configFile == null ) {
+            configFile = new File( plugin.getDataFolder(), "config.yml" );
+        }
+        if ( configFile.exists() == false ) {
+            plugin.saveResource( "config.yml", false );
+        }
+
+        try { ConfigUpdater.update( plugin, "config.yml", configFile, Collections.emptyList() );
+        } catch ( IOException e ) { e.printStackTrace(); }
+
+        reloadConfig();
     }
 }
