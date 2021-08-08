103d89f8f27e9652e0732631652667cac074f708
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-08-07 23:21:56 -0500

    Now uses the primary and secondary colors from the config file

--- src/main/java/com/github/cyberryan1/netuno/utils/CommandErrors.java
+++ src/main/java/com/github/cyberryan1/netuno/utils/CommandErrors.java
@@ -5,15 +5,15 @@ import org.bukkit.command.CommandSender;
 public class CommandErrors {
 
     public static void sendInvalidTimespan( CommandSender sender, String time ) {
-        sender.sendMessage( Utils.getColored( "&7Invalid timespan &8(&6\"" + time + "&6\"&8)" ) );
+        sender.sendMessage( Utils.getColored( "&hInvalid timespan &8(&g\"" + time + "&g\"&8)" ) );
     }
 
     public static void sendPlayerNotFound( CommandSender sender, String player ) {
-        sender.sendMessage( Utils.getColored( "&7No player with the name &6" + player + " &7found" ) );
+        sender.sendMessage( Utils.getColored( "&hNo player with the name &g" + player + " &hfound" ) );
     }
 
     public static void sendPlayerNeverJoined( CommandSender sender, String player ) {
-        sender.sendMessage( Utils.getColored( "&7Player with the name &6" + player + " &7has never joined the server before" ) );
+        sender.sendMessage( Utils.getColored( "&hPlayer with the name &g" + player + " &hhas never joined the server before" ) );
     }
 
     public static void sendInvalidPerms( CommandSender sender ) {
@@ -21,7 +21,7 @@ public class CommandErrors {
     }
 
     public static void sendPlayerCannotBePunished( CommandSender sender, String target ) {
-        sender.sendMessage( Utils.getColored( "&6" + target + " &7is a staff member, so they cannot be punished" ) );
+        sender.sendMessage( Utils.getColored( "&g" + target + " &his a staff member, so they cannot be punished" ) );
     }
 
     public static void sendCommandUsage( CommandSender sender, String command ) {
@@ -31,57 +31,57 @@ public class CommandErrors {
     public static String getCommandUsage( String command ) {
         switch ( command.toLowerCase() ) {
             case "warn":
-                return Utils.getColored( "&8/&6warn &7(player) (reason)" );
+                return Utils.getColored( "&8/&gwarn &h(player) (reason)" );
             case "clearchat":
-                return Utils.getColored( "&8/&6clearchat" );
+                return Utils.getColored( "&8/&gclearchat" );
             case "kick":
-                return Utils.getColored( "&8/&6kick &7(player) (reason)" );
+                return Utils.getColored( "&8/&gkick &h(player) (reason)" );
             case "mute":
-                return Utils.getColored( "&8/&6mute &7(player) (time) (reason) " );
+                return Utils.getColored( "&8/&gmute &h(player) (time) (reason) " );
             case "unmute":
-                return Utils.getColored( "&8/&6unmute &7(player)" );
+                return Utils.getColored( "&8/&gunmute &h(player)" );
             case "ban":
-                return Utils.getColored( "&8/&6ban &7(player) (time) (reason)" );
+                return Utils.getColored( "&8/&gban &h(player) (time) (reason)" );
             case "unban":
-                return Utils.getColored( "&8/&6unban &7(player) (time) (reason)" );
+                return Utils.getColored( "&8/&gunban &h(player) (time) (reason)" );
             case "ipinfo":
-                return Utils.getColored( "&8/&6ipinfo &7(player)" );
+                return Utils.getColored( "&8/&gipinfo &h(player)" );
             case "ipmute":
-                return Utils.getColored( "&8/&6ipmute &7(player) (time) (reason)" );
+                return Utils.getColored( "&8/&gipmute &h(player) (time) (reason)" );
             case "unipmute":
-                return Utils.getColored( "&8/&6unipmute &7(player)" );
+                return Utils.getColored( "&8/&gunipmute &h(player)" );
             case "ipban":
-                return Utils.getColored( "&8/&6ipban &7(player) (time) (reason)" );
+                return Utils.getColored( "&8/&gipban &h(player) (time) (reason)" );
             case "unipban":
-                return Utils.getColored( "&8/&6unipban &7(player)" );
+                return Utils.getColored( "&8/&gunipban &h(player)" );
             case "history":
                 return getCommandUsage( "history-list" ) + "\n" + getCommandUsage( "history-edit" ) + "\n" + getCommandUsage( "history-reset" );
             case "history-list":
-                return Utils.getColored( "&8/&6history &7list (player)" );
+                return Utils.getColored( "&8/&ghistory &hlist (player)" );
             case "history-edit":
-                return Utils.getColored( "&8/&6history &7edit (pun ID)" );
+                return Utils.getColored( "&8/&ghistory &hedit (pun ID)" );
             case "history-reset":
-                return Utils.getColored( "&8/&6history &7reset (player)" );
+                return Utils.getColored( "&8/&ghistory &hreset (player)" );
             case "togglesigns":
-                return Utils.getColored( "&8/&6togglesigns &7[enable/disable]" );
+                return Utils.getColored( "&8/&gtogglesigns &h[enable/disable]" );
             case "mutechat":
-                return Utils.getColored( "&8/&6mutechat &7[enable/disable/toggle/status]" );
+                return Utils.getColored( "&8/&gmutechat &h[enable/disable/toggle/status]" );
             case "report":
-                return Utils.getColored( "&8/&6report &7(player)" );
+                return Utils.getColored( "&8/&greport &h(player)" );
             case "reports":
-                return Utils.getColored( "&8/&6reports &7[player]" );
+                return Utils.getColored( "&8/&greports &h[player]" );
             case "help":
-                return Utils.getColored( "&8/&6netuno &7help [page]" );
+                return Utils.getColored( "&8/&gnetuno &hhelp [page]" );
             case "reload":
-                return Utils.getColored( "&8/&6netuno &7reload" );
+                return Utils.getColored( "&8/&gnetuno &hreload" );
             case "punish":
-                return Utils.getColored( "&8/&6punish &7(player)" );
+                return Utils.getColored( "&8/&gpunish &h(player)" );
             case "chatslow":
                 return getCommandUsage( "chatslow-get" ) + "\n" + getCommandUsage( "chatslow-set" );
             case "chatslow-get":
-                return Utils.getColored( "&8/&6chatslow &7get" );
+                return Utils.getColored( "&8/&gchatslow &hget" );
             case "chatslow-set":
-                return Utils.getColored( "&8/&6chatslow &7set (amount)");
+                return Utils.getColored( "&8/&gchatslow &hset (amount)");
         }
 
         return null;
@@ -105,23 +105,23 @@ public class CommandErrors {
                 break;
         }
 
-        sender.sendMessage( Utils.getColored( "&6" + target + " &7does not have any active " + plural ) );
+        sender.sendMessage( Utils.getColored( "&g" + target + " &hdoes not have any active " + plural ) );
     }
 
     public static void sendNoAltAccounts( CommandSender sender, String target ) {
-        sender.sendMessage( Utils.getColored( "&6" + target + " &7has no other alt accounts" ) );
+        sender.sendMessage( Utils.getColored( "&g" + target + " &hhas no other alt accounts" ) );
     }
 
     public static void sendInvalidPageNumber( CommandSender sender, String page ) {
-        sender.sendMessage( Utils.getColored( "&7Invalid page number &8(&6\"" + page + "&6\"&8)" ) );
+        sender.sendMessage( Utils.getColored( "&hInvalid page number &8(&g\"" + page + "&g\"&8)" ) );
     }
 
     public static void sendHistoryPageNoExist( CommandSender sender, String targetName, int maxPage ) {
-        sender.sendMessage( Utils.getColored( "&7Maximum page number for &6" + targetName + " &7is &6" + maxPage ) );
+        sender.sendMessage( Utils.getColored( "&hMaximum page number for &g" + targetName + " &his &g" + maxPage ) );
     }
 
     public static void sendNoPreviousPunishments( CommandSender sender, String targetName ) {
-        sender.sendMessage( Utils.getColored( "&6" + targetName + " &7has no previous punishments" ) );
+        sender.sendMessage( Utils.getColored( "&g" + targetName + " &hhas no previous punishments" ) );
     }
 
     public static void sendCanOnlyBeRanByPlayer( CommandSender sender ) {
@@ -129,34 +129,34 @@ public class CommandErrors {
     }
 
     public static void sendUnexpectedError( CommandSender sender ) {
-        sender.sendMessage( Utils.getColored( "&6&lNETUNO ERROR! &7An unexpected error occurred. Please contact the plugin developer about this!" ) );
+        sender.sendMessage( Utils.getColored( "&g&lNETUNO ERROR! &hAn unexpected error occurred. Please contact the plugin developer about this!" ) );
     }
 
     public static void sendConfigError( CommandSender sender ) {
-        sender.sendMessage( Utils.getColored( "&7An error occurred because the config file is not setup correctly. Please contact a server administrator about this!" ) );
+        sender.sendMessage( Utils.getColored( "&hAn error occurred because the config file is not setup correctly. Please contact a server administrator about this!" ) );
     }
 
     public static void sendInvalidPunishmentID( CommandSender sender, String arg ) {
-        sender.sendMessage( Utils.getColored( "&7Invalid punishment ID &8(&6\"" + arg + "&6\"&8)" ) );
+        sender.sendMessage( Utils.getColored( "&hInvalid punishment ID &8(&g\"" + arg + "&g\"&8)" ) );
     }
 
     public static void sendPunishmentIDNotFound( CommandSender sender, int id ) {
-        sender.sendMessage( Utils.getColored( "&7Punishment with ID &6" + id + " &7not found" ) );
+        sender.sendMessage( Utils.getColored( "&hPunishment with ID &g" + id + " &hnot found" ) );
     }
 
     public static void sendPlayerExempt( CommandSender sender, String targetName ) {
-        sender.sendMessage( Utils.getColored( "&6" + targetName + " &7is exempt from this!" ) );
+        sender.sendMessage( Utils.getColored( "&g" + targetName + " &his exempt from this!" ) );
     }
 
     public static void sendReportNeedsOneReason( CommandSender sender ) {
-        sender.sendMessage( Utils.getColored( "&7You must provide at least 1 reason in your report" ) );
+        sender.sendMessage( Utils.getColored( "&hYou must provide at least 1 reason in your report" ) );
     }
 
     public static void sendTargetAlreadyBeingPunished( CommandSender sender, String targetName, String staffPunishingName ) {
-        sender.sendMessage( Utils.getColored( "&6" + targetName + " &7is already being punished by &6" + staffPunishingName ) );
+        sender.sendMessage( Utils.getColored( "&g" + targetName + " &his already being punished by &g" + staffPunishingName ) );
     }
 
     public static void sendTargetHasNoPunishedAlts( CommandSender sender, String targetName ) {
-        sender.sendMessage( Utils.getColored( "&6" + targetName + " &7has no punished alts" ) );
+        sender.sendMessage( Utils.getColored( "&g" + targetName + " &hhas no punished alts" ) );
     }
 }
