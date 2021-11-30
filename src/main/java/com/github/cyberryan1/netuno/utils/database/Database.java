7437fb2698a85f841a453be685f2df1ffeafdbec
CyberRyan <cyberryan934@gmail.com>
2021-11-29 19:37:32 -0600

    Added a temp fix for the server crashing due to "connection being closed"

--- src/main/java/com/github/cyberryan1/netuno/utils/database/Database.java
+++ src/main/java/com/github/cyberryan1/netuno/utils/database/Database.java
@@ -73,13 +73,14 @@ public abstract class Database {
     }
 
     public void close( Connection conn, PreparedStatement ps, ResultSet rs ) {
-        try {
-            if ( conn != null ) { conn.close(); }
-            if ( ps != null ) { ps.close(); }
-            if ( rs != null ) { rs.close(); }
-        } catch ( SQLException e ) {
-            Error.close( plugin, e );
-        }
+        return; // ? bug fix?
+//        try {
+//            if ( conn != null ) { conn.close(); }
+//            if ( ps != null ) { ps.close(); }
+//            if ( rs != null ) { rs.close(); }
+//        } catch ( SQLException e ) {
+//            Error.close( plugin, e );
+//        }
     }
 
     //
