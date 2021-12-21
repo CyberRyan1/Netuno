61e8076522deab343703e20fc9e01cbe68783a4b
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-12-20 21:39:01 -0600

    Using BaseCommand class

--- src/main/java/com/github/cyberryan1/netuno/commands/Report.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/Report.java
@@ -1,5 +1,6 @@
 package com.github.cyberryan1.netuno.commands;
 
+import com.github.cyberryan1.netuno.classes.BaseCommand;
 import com.github.cyberryan1.netuno.guis.report.ReportGUI;
 import com.github.cyberryan1.netuno.utils.CommandErrors;
 import com.github.cyberryan1.netuno.utils.ConfigUtils;
@@ -12,7 +13,33 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Report implements CommandExecutor {
+import java.util.Collections;
+import java.util.List;
+
+public class Report extends BaseCommand {
+
+    public Report() {
+        super( "report", ConfigUtils.getStr( "report.perm" ), getColorizedStr( "&8/&ureport &y(player)") );
+    }
+
+    @Override
+    public boolean permissionsAllowed( CommandSender sender ) {
+        if ( super.permission.equals( "" ) ) { return true; }
+        else { return VaultUtils.hasPerms( sender, super.permission ); }
+    }
+
+    @Override
+    public List<String> onTabComplete( CommandSender sender, Command command, String label, String[] args ) {
+        if ( permissionsAllowed( sender ) ) {
+            if ( args.length == 0 || args[0].length() == 0 ) {
+                return getAllOnlinePlayerNames();
+            }
+            else if ( args.length == 1 ) {
+                return matchOnlinePlayers( args[0] );
+            }
+        }
+        return Collections.emptyList();
+    }
 
     @Override
     // /report (player)
