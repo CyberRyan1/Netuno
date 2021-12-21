68bd5064e9c13c754f649aed6894507e16a943a8
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-12-20 21:24:23 -0600

    Using BaseCommand class

--- src/main/java/com/github/cyberryan1/netuno/commands/NetunoCmd.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/NetunoCmd.java
@@ -1,14 +1,46 @@
 package com.github.cyberryan1.netuno.commands;
 
+import com.github.cyberryan1.netuno.classes.BaseCommand;
 import com.github.cyberryan1.netuno.utils.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class NetunoCmd implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NetunoCmd extends BaseCommand {
 
     private final String COMMAND_ORDER[] = { "help", "punish", "warn", "clearchat", "kick", "mute", "unmute", "history", "ban", "unban",
             "ipinfo", "ipmute", "unipmute", "ipban", "unipban", "report", "reports", "togglesigns", "reload" };
+
+    public NetunoCmd() {
+        super( "netuno", ConfigUtils.getStr( "general.staff-perm" ) );
+    }
+
+    @Override
+    public List<String> onTabComplete( CommandSender sender, Command command, String label, String[] args ) {
+        if ( permissionsAllowed( sender ) ) {
+            if ( args.length == 0 || args[0].length() == 0 ) {
+                List<String> toReturn = new ArrayList<>();
+                Collections.addAll( toReturn, "reload", "help" );
+                return toReturn;
+            }
+            else if ( "RELOAD".startsWith( args[0].toUpperCase()  ) ) {
+                List<String> toReturn = new ArrayList<>();
+                Collections.addAll( toReturn, "reload" );
+                return toReturn;
+            }
+            else if ( "HELP".startsWith( args[0].toUpperCase() ) ) {
+                List<String> toReturn = new ArrayList<>();
+                Collections.addAll( toReturn, "help" );
+                return toReturn;
+            }
+        }
+
+        return Collections.emptyList();
+    }
 
     @Override
     public boolean onCommand( CommandSender sender, Command command, String label, String args[] ) {
