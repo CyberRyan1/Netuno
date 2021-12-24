2d2d0ed1816083c0a705f1b036925711f25c75a4
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2021-12-24 00:58:50 -0600

    Added a way to get the java version

--- src/main/java/com/github/cyberryan1/netuno/utils/Utils.java
+++ src/main/java/com/github/cyberryan1/netuno/utils/Utils.java
@@ -257,4 +257,15 @@ public class Utils {
             default: return null;
         }
     }
+
+    // gets the current java version
+    public static int getJavaVersion() {
+        String version = System.getProperty("java.version");
+        if(version.startsWith("1.")) {
+            version = version.substring(2, 3);
+        } else {
+            int dot = version.indexOf(".");
+            if(dot != -1) { version = version.substring(0, dot); }
+        } return Integer.parseInt(version);
+    }
 }
