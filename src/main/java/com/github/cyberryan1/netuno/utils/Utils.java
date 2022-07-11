93aa6ce88272010ac82f8eaa75ad4d1bf48a1ca9
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-10 21:13:51 -0500

    Added Utils

---
+++ src/main/java/com/github/cyberryan1/netuno/utils/Utils.java
@@ -0,0 +1,9 @@
+package com.github.cyberryan1.netuno.utils;
+
+public class Utils {
+
+    // Returns the current timestamp, in seconds
+    public static long getCurrentTimestamp() {
+        return System.currentTimeMillis() / 1000L;
+    }
+}
