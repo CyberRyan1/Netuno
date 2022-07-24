a37b1d9b8e9841d1a40cdc474936746b8daf79a7
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-24 17:33:17 -0500

    Added Settings and SettingsEntry

---
+++ src/main/java/com/github/cyberryan1/netuno/utils/settings/Settings.java
@@ -0,0 +1,228 @@
+package com.github.cyberryan1.netuno.utils.settings;
+
+import com.github.cyberryan1.cybercore.utils.CoreUtils;
+import org.bukkit.Material;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public enum Settings {
+
+    //
+    // General Section
+    //
+
+    STAFF_PERMISSION( "general.staff-perm", "string" ),
+    ALL_PERMISSIONS( "general.all-perms", "string" ),
+    PRIMARY_COLOR( "general.primary-color", "string" ),
+    SECONDARY_COLOR( "general.secondary-color", "string" ),
+    SILENT_PREFIX( "general.silent-prefix", "string" ),
+    SILENT_PERMISSION( "general.silent-perm", "string" ),
+    STAFF_PUNISHMENTS( "general.staff-punishments", "boolean" ),
+    RELOAD_PERMISSION( "general.reload-perm", "string" ),
+
+    //
+    // Moderation Commands
+    //
+
+    // Warn Command
+    WARN_PERMISSION( "warn.perm", "string" ),
+    WARN_BROADCAST( "warn.broadcast", "strlist" ),
+    WARN_STAFF_BROADCAST( "warn.staff-broadcast", "strlist" ),
+    WARN_MESSAGE( "warn.message", "strlist" ),
+
+    // Kick command
+    KICK_PERMISSION( "kick.perm", "string" ),
+    KICK_BROADCAST( "kick.broadcast", "strlist" ),
+    KICK_STAFF_BROADCAST( "kick.staff-broadcast", "strlist" ),
+    KICK_KICKED_LINES( "kick.kicked-lines", "strlist" ),
+
+    // Mute command
+    MUTE_PERMISSION( "mute.perm", "string" ),
+    MUTE_BROADCAST( "mute.broadcast", "strlist" ),
+    MUTE_STAFF_BROADCAST( "mute.staff-broadcast", "strlist" ),
+    MUTE_MESSAGE( "mute.message", "strlist" ),
+    MUTE_ATTEMPT( "mute.attempt", "strlist" ),
+    MUTE_EXPIRE( "mute.expire", "strlist" ),
+    MUTE_EXPIRE_STAFF( "mute.expire-staff", "strlist" ),
+    MUTE_BLOCKED_COMMANDS( "mute.blocked-commands", "strlist" ),
+    MUTE_BLOCKED_COMMAND_MESSAGE( "mute.blocked-command-message", "str" ),
+
+    // Unmute command
+    UNMUTE_PERMISSION( "unmute.perm", "string" ),
+    UNMUTE_BROADCAST( "unmute.broadcast", "strlist" ),
+    UNMUTE_STAFF_BROADCAST( "unmute.staff-broadcast", "strlist" ),
+    UNMUTE_MESSAGE( "unmute.message", "strlist" ),
+
+    // Ban command
+    BAN_PERMISSION( "ban.perm", "string" ),
+    BAN_BROADCAST( "ban.broadcast", "strlist" ),
+    BAN_MAX_TIME_ENABLED( "ban.max-time-enabled", "boolean" ),
+    BAN_MAX_TIME_LENGTH( "ban.max-time-length", "string" ),
+    BAN_MAX_TIME_BYPASS_PERMISSION( "ban.max-time-bypass-perm", "string" ),
+    BAN_STAFF_BROADCAST( "ban.staff-broadcast", "strlist" ),
+    BAN_MESSAGE( "ban.banned-lines", "strlist" ),
+    BAN_ATTEMPT( "ban.attempt", "strlist" ),
+    BAN_EXPIRE( "ban.expire", "strlist" ),
+    BAN_EXPIRE_STAFF( "ban.expire-staff", "strlist" ),
+
+    // Unban command
+    UNBAN_PERMISSION( "unban.perm", "string" ),
+    UNBAN_BROADCAST( "unban.broadcast", "strlist" ),
+    UNBAN_STAFF_BROADCAST( "unban.staff-broadcast", "strlist" ),
+    UNBAN_MESSAGE( "unban.message", "strlist" ),
+
+    // Ipmute command
+    IPMUTE_PERMISSION( "ipmute.perm", "string" ),
+    IPMUTE_BROADCAST( "ipmute.broadcast", "strlist" ),
+    IPMUTE_STAFF_BROADCAST( "ipmute.staff-broadcast", "strlist" ),
+    IPMUTE_MESSAGE( "ipmute.message", "strlist" ),
+    IPMUTE_ATTEMPT( "ipmute.attempt", "strlist" ),
+    IPMUTE_EXPIRE( "ipmute.expire", "strlist" ),
+    IPMUTE_EXPIRE_STAFF( "ipmute.expire-staff", "strlist" ),
+
+    // Ipunmute command
+    UNIPMUTE_PERMISSION( "unipmute.perm", "string" ),
+    UNIPMUTE_BROADCAST( "unipmute.broadcast", "strlist" ),
+    UNIPMUTE_STAFF_BROADCAST( "unipmute.staff-broadcast", "strlist" ),
+    UNIPMUTE_MESSAGE( "unipmute.message", "strlist" ),
+
+    // Ipban command
+    IPBAN_PERMISSION( "ipban.perm", "string" ),
+    IPBAN_BROADCAST( "ipban.broadcast", "strlist" ),
+    IPBAN_MESSAGE( "ipban.banned-lines", "strlist" ),
+    IPBAN_ATTEMPT( "ipban.attempt", "strlist" ),
+    IPBAN_EXPIRE( "ipban.expire", "strlist" ),
+    IPBAN_EXPIRE_STAFF( "ipban.expire-staff", "strlist" ),
+
+    // Unipban command
+    UNIPBAN_PERMISSION( "unipban.perm", "string" ),
+    UNIPBAN_BROADCAST( "unipban.broadcast", "strlist" ),
+    UNIPBAN_STAFF_BROADCAST( "unipban.staff-broadcast", "strlist" ),
+    UNIPBAN_MESSAGE( "unipban.message", "strlist" ),
+
+    // Punish command
+    PUNISH_PERMISSION( "punish.perm", "string" ),
+    PUNISH_OTHER_STAFF( "punish.staff-punish", "boolean" ),
+
+    //
+    // Alt Management
+    //
+
+    // Ipinfo command
+    IPINFO_PERMISSION( "ipinfo.perm", "string" ),
+    IPINFO_EXEMPT_PERMISSION( "ipinfo.exempt-perm", "string" ),
+    IPINFO_NOTIFS( "ipinfo.notifs", "boolean" ),
+    IPINFO_NOTIFS_MESSAGE( "ipinfo.notifs-message", "strlist" ),
+    IPINFO_NOTIF_HOVER_MESSAGE( "ipinfo.notif-hover", "string" ),
+
+    //
+    // Chat Management
+    //
+
+    // Mutechat command
+    MUTECHAT_PERMISSION( "mutechat.perm", "string" ),
+    MUTECHAT_BYPASS_PERMISSION( "mutechat.bypass-perm", "string" ),
+    MUTECHAT_CHAT_ENABLE_BROADCAST( "mutechat.enable-broadcast", "strlist" ),
+    MUTECHAT_CHAT_DISABLE_BROADCAST( "mutechat.disable-broadcast", "strlist" ),
+    MUTECHAT_ATTEMPT( "mutechat.attempt", "strlist" ),
+
+    // Clearchat command
+    CLEARCHAT_PERMISSION( "clearchat.perm", "string" ),
+    CLEARCHAT_BROADCAST( "clearchat.broadcast", "string" ),
+    CLEARCHAT_STAFF_BYPASS( "clearchat.staff-bypass", "boolean" ),
+    CLEARCHAT_STAFF_BROADCAST( "clearchat.staff-broadcast", "string" ),
+
+    // Chatslow command
+    CHATSLOW_PERMISSION( "chatslow.perm", "string" ),
+    CHATSLOW_BYPASS_PERMISSION( "chatslow.bypass-perm", "string" ),
+    CHATSLOW_DEFAULT_VALUE( "chatslow.default-value", "int" ),
+    CHATSLOW_BROADCAST( "chatslow.broadcast", "string" ),
+    CHATSLOW_MESSAGE( "chatslow.message", "string" ),
+
+    //
+    // History
+    //
+    HISTORY_PERMISSION( "history.perm", "string" ),
+    HISTORY_REASON_PERMISSION( "history.reason.perm", "string" ),
+    HISTORY_TIME_PERMISSION( "history.time.perm", "string" ),
+    HISTORY_DELETE_PERMISSION( "history.delete.perm", "string" ),
+    HISTORY_RESET_PERMISSION( "history.reset.perm", "string" ),
+
+    //
+    // Signs
+    //
+    ALLOW_SIGNS_WHILE_PUNISHED( "signs.allow-while-muted", "boolean" ),
+    SIGN_WHILE_PUNISHED_MESSAGE( "signs.while-muted-ATTEMPT", "string" ),
+    SIGN_NOTIFS_ENABLED( "signs.notifs", "boolean" ),
+    SIGN_NOTIFS_PERMISSION( "signs.notifs-perm", "string" ),
+    SIGN_NOTIFS_MESSAGE( "sign.notifs-msg", "strlist" ),
+
+    //
+    // Reports
+    //
+    REPORT_PERMISSION( "report.perm", "string" ),
+    REPORT_REASONS_LIST( "report.reasons", "strlist" ),
+    REPORT_CONFIRM_MESSAGE( "report.confirm-msg", "strlist" ),
+    REPORT_STAFF_MESSAGE( "report.staff-msg", "strlist" ),
+    REPORT_VIEW_PERMISSION( "reports.perm", "string" ),
+    REPORT_EXPIRE_TIME( "reports.delete-after", "int" ),
+
+    // * Special note: Anything related to the punishment GUI is done in it's own file
+
+    //
+    // Database
+    //
+    DATABASE_USE_SQLITE( "database.use-sqlite", "boolean" ),
+    DATABASE_SQL_HOST( "database.sql.host", "string" ),
+    DATABASE_SQL_PORT( "database.sql.port", "int" ),
+    DATABASE_SQL_DATABASE( "database.sql.database", "string" ),
+    DATABASE_SQL_USERNAME( "database.sql.userNAME", "string" ),
+    DATABASE_SQL_PASSWORD( "database.sql.password", "string" ),
+
+    CACHE_EDITS_BEFORE_SAVE( "database.cache.edits-before-save", "int" )
+
+    ;
+
+    private String path;
+    private SettingsEntry value;
+    Settings( String path, String valueType ) {
+        this.path = path;
+        this.value = new SettingsEntry( path, valueType );
+    }
+
+    public String getPath() { return this.path; }
+
+    public SettingsEntry getValue() { return this.value; }
+
+    public int integer() { return value.integer(); }
+
+    public String string() { return value.string(); }
+
+    public String coloredString() { return CoreUtils.getColored( value.string() ); }
+
+    public float getFloat() { return value.getFloat(); }
+
+    public double getDouble() { return value.getDouble(); }
+
+    public long getLong() { return value.getLong(); }
+
+    public boolean bool() { return value.bool(); }
+
+    public Material material() { return value.material(); }
+
+    public String[] stringlist() { return value.stringlist(); }
+
+    public String[] coloredStringlist() {
+        String[] toReturn = new String[ stringlist().length ];
+        for ( int i = 0; i < stringlist().length; i++ ) {
+            toReturn[i] = CoreUtils.getColored( stringlist()[i] );
+        }
+        return toReturn;
+    }
+
+    public List<String> arraylist() { return new ArrayList<>( Arrays.asList( stringlist() ) ); }
+
+    public List<String> coloredArraylist() { return new ArrayList<>( Arrays.asList( coloredStringlist() ) ); }
+}
---
+++ src/main/java/com/github/cyberryan1/netuno/utils/settings/SettingsEntry.java
@@ -0,0 +1,55 @@
+package com.github.cyberryan1.netuno.utils.settings;
+
+import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
+import org.bukkit.Material;
+
+public class SettingsEntry {
+
+    //
+    // Class methods
+    //
+
+    private String path;
+    private int i;
+    private String str;
+    private float f;
+    private double d;
+    private long l;
+    private boolean b;
+    private Material mat;
+    private String strList[];
+
+
+    public SettingsEntry( String path, String valueType ) {
+        this.path = path;
+
+        switch ( valueType.toLowerCase() ) {
+            case "int" -> this.i = YMLUtils.getConfig().getInt( path );
+            case "string" -> this.str = YMLUtils.getConfig().getStr( path );
+            case "float" -> this.f = YMLUtils.getConfig().getFloat( path );
+            case "double" -> this.d = YMLUtils.getConfig().getDouble( path );
+            case "long" -> this.l = YMLUtils.getConfig().getYMLManager().getConfig().getLong( path );
+            case "boolean" -> this.b = YMLUtils.getConfig().getBool( path );
+            case "material" -> this.mat = Material.valueOf( YMLUtils.getConfig().getStr( path ) );
+            case "strlist" -> this.strList = YMLUtils.getConfig().getStrList( path );
+        }
+    }
+
+    public String getPath() { return this.path; }
+
+    public int integer() { return this.i; }
+
+    public String string() { return this.str; }
+
+    public float getFloat() { return this.f; }
+
+    public double getDouble() { return this.d; }
+
+    public long getLong() { return this.l; }
+
+    public boolean bool() { return this.b; }
+
+    public Material material() { return this.mat; }
+
+    public String[] stringlist() { return this.strList; }
+}
