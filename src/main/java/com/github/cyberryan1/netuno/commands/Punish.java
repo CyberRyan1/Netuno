57d62b6ba45efd0358513736ecaa44d5a585dfd4
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-30 16:45:24 -0500

    Fully refactored to use CyberCore

--- src/main/java/com/github/cyberryan1/netuno/commands/Punish.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/Punish.java
@@ -1,84 +1,55 @@
 package com.github.cyberryan1.netuno.commands;
 
-import com.github.cyberryan1.cybercore.utils.VaultUtils;
-import com.github.cyberryan1.netuno.classes.BaseCommand;
+import com.github.cyberryan1.cybercore.helpers.command.ArgType;
+import com.github.cyberryan1.cybercore.helpers.command.CyberCommand;
 import com.github.cyberryan1.netuno.guis.punish.MainPunishGUI;
 import com.github.cyberryan1.netuno.utils.CommandErrors;
 import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
+import com.github.cyberryan1.netuno.utils.settings.Settings;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Collections;
 import java.util.List;
 
-public class Punish extends BaseCommand {
+public class Punish extends CyberCommand {
 
     public Punish() {
-        super( "punish", YMLUtils.getConfig().getStr( "punish.perm" ), getColorizedStr( "&8/&upunish &y(player)") );
+        super(
+                "punish",
+                Settings.PUNISH_PERMISSION.string(),
+                Settings.PERM_DENIED_MSG.string(),
+                "&8/&spunish &p(player)"
+        );
+        register( true );
+
+        demandPermission( true );
+        demandPlayer( true );
+        setMinArgs( 1 );
+        setArgType( 0, ArgType.OFFLINE_PLAYER );
     }
 
     @Override
-    public List<String> onTabComplete( CommandSender sender, Command command, String label, String[] args ) {
-        if ( permissionsAllowed( sender ) ) {
-            if ( args.length == 0 || args[0].length() == 0 ) {
-                return getAllOnlinePlayerNames();
-            }
-            else if ( args.length == 1 ) {
-                return matchOnlinePlayers( args[0] );
-            }
-        }
-        return Collections.emptyList();
+    public List<String> tabComplete( CommandSender sender, String[] args ) {
+        return List.of();
     }
 
     @Override
     // /punish (player)
-    public boolean onCommand( CommandSender sender, Command command, String label, String args[] ) {
+    public boolean execute( CommandSender sender, String args[] ) {
+        final Player staff = ( Player ) sender;
+        final OfflinePlayer target = Bukkit.getOfflinePlayer( args[0] );
 
-        if ( VaultUtils.hasPerms( sender, YMLUtils.getConfig().getStr( "punish.perm" ) ) == false ) {
-            CommandErrors.sendInvalidPerms( sender );
-            return true;
-        }
-
-        if ( sender instanceof Player == false ) {
-            CommandErrors.sendCanOnlyBeRanByPlayer( sender );
-            return true;
-        }
-        Player staff = ( Player ) sender;
-
-        if ( Utils.isOutOfBounds( args, 0 ) == false ) {
-
-            if ( Utils.isValidUsername( args[0] ) == false ) {
-                CommandErrors.sendPlayerNotFound( sender, args[0] );
+        if ( Settings.PUNISH_OTHER_STAFF.bool() == false ) {
+            if ( Utils.checkStaffPunishmentAllowable( staff, target ) == false ) {
+                CommandErrors.sendPlayerCannotBePunished( staff, target.getName() );
                 return true;
             }
-
-            OfflinePlayer target = Bukkit.getOfflinePlayer( args[0] );
-            if ( target != null ) {
-
-                if ( YMLUtils.getConfig().getBool( "punish.staff-punish" ) == false ) {
-                    if ( Utils.checkStaffPunishmentAllowable( staff, target ) == false ) {
-                        CommandErrors.sendPlayerCannotBePunished( staff, target.getName() );
-                        return true;
-                    }
-                }
-
-                MainPunishGUI gui = new MainPunishGUI( staff, target );
-                gui.openInventory();
-            }
-
-            else {
-                CommandErrors.sendPlayerNotFound( sender, args[0] );
-            }
         }
 
-        else {
-            CommandErrors.sendCommandUsage( sender, "punish" );
-        }
-
+        MainPunishGUI gui = new MainPunishGUI( staff, target );
+        gui.open();
         return true;
     }
 }
