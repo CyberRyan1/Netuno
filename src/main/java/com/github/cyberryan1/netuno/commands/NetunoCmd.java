5dccfda12bf11ca2b28e320a697cf1ea0c206695
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-30 13:12:32 -0500

    Fully refactored to use CyberCore

--- src/main/java/com/github/cyberryan1/netuno/commands/NetunoCmd.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/NetunoCmd.java
@@ -1,96 +1,79 @@
 package com.github.cyberryan1.netuno.commands;
 
+import com.github.cyberryan1.cybercore.helpers.command.CyberCommand;
+import com.github.cyberryan1.cybercore.utils.CoreUtils;
 import com.github.cyberryan1.cybercore.utils.VaultUtils;
-import com.github.cyberryan1.netuno.classes.BaseCommand;
-import com.github.cyberryan1.netuno.utils.CommandErrors;
+import com.github.cyberryan1.netuno.Netuno;
 import com.github.cyberryan1.netuno.utils.Utils;
+import com.github.cyberryan1.netuno.utils.settings.Settings;
 import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class NetunoCmd extends BaseCommand {
+public class NetunoCmd extends CyberCommand {
 
     private final String COMMAND_ORDER[] = { "help", "punish", "warn", "clearchat", "kick", "mute", "unmute", "history", "ban", "unban",
             "ipinfo", "ipmute", "unipmute", "ipban", "unipban", "report", "reports", "togglesigns", "reload" };
 
     public NetunoCmd() {
-        super( "netuno", YMLUtils.getConfig().getStr( "general.staff-perm" ) );
+        super(
+                "netuno [reload/help] [page]",
+                Settings.STAFF_PERMISSION.string(),
+                Settings.PERM_DENIED_MSG.string(),
+                null
+        );
+        register( true );
+
+        demandPermission( true );
     }
 
     @Override
-    public List<String> onTabComplete( CommandSender sender, Command command, String label, String[] args ) {
+    public List<String> tabComplete( CommandSender sender, String[] args ) {
         if ( permissionsAllowed( sender ) ) {
-            if ( args.length == 0 || args[0].length() == 0 ) {
-                List<String> toReturn = new ArrayList<>();
-                Collections.addAll( toReturn, "reload", "help" );
-                return toReturn;
-            }
-            else if ( "RELOAD".startsWith( args[0].toUpperCase()  ) ) {
-                List<String> toReturn = new ArrayList<>();
-                Collections.addAll( toReturn, "reload" );
-                return toReturn;
-            }
-            else if ( "HELP".startsWith( args[0].toUpperCase() ) ) {
-                List<String> toReturn = new ArrayList<>();
-                Collections.addAll( toReturn, "help" );
-                return toReturn;
-            }
+            List<String> suggestions = List.of( "reload", "help" );
+            if ( args.length == 0 || args[0].length() == 0 ) { return suggestions; }
+            if ( args.length == 1 ) { return matchArgs( suggestions, args[0] ); }
         }
 
-        return Collections.emptyList();
+        return List.of();
     }
 
     @Override
-    public boolean onCommand( CommandSender sender, Command command, String label, String args[] ) {
+    public boolean execute( CommandSender sender, String args[] ) {
+        if ( args.length == 0 || args[0].equalsIgnoreCase( "help" ) ) {
+            if ( args.length > 1 ) {
+                int page = 0;
+                try { page = Integer.parseInt( args[1] ) - 1; }
+                catch ( NumberFormatException e ) { CoreUtils.sendMsg( sender, "&sInvalid page number!" ); }
 
-        if ( VaultUtils.hasPerms( sender, YMLUtils.getConfig().getStr( "general.staff-perm" ) ) == false ) {
-            CommandErrors.sendInvalidPerms( sender );
-            return true;
-        }
-
-        if ( Utils.isOutOfBounds( args, 0 ) == false ) {
-            if ( args[0].equalsIgnoreCase( "reload" ) ) {
-                if ( VaultUtils.hasPerms( sender, YMLUtils.getConfig().getStr( "general.reload-perm" ) ) ) {
-                    sender.sendMessage( Utils.getColored( "&7Attempting to reload &6Netuno&7..." ) );
-                    Utils.logInfo( "Attempting to reload Netuno..." );
-
-                    YMLUtils.getConfig().getYMLManager().initialize();
-
-                    sender.sendMessage( Utils.getColored( "&7Successfully reloaded &6Netuno" ) );
-                    Utils.logInfo( "Successfully reloaded Netuno and its files" );
+                if ( page >= 0 && page < Math.ceil( Netuno.registeredCommands.size() / 6.0 ) ) {
+                    sendHelpMessage( sender, page * 6, page * 6 + 6 );
                 }
 
                 else {
-                    CommandErrors.sendInvalidPerms( sender );
-                }
-            }
-
-            else if ( args[0].equalsIgnoreCase( "help" ) ) {
-                if ( Utils.isOutOfBounds( args, 1 ) == false ) {
-                    int page = 0;
-                    try { page = Integer.parseInt( args[1] ) - 1; }
-                    catch ( NumberFormatException e ) { sender.sendMessage( Utils.getColored( "&hInvalid page number!" ) ); }
-
-                    if ( page >= 0 && page < Math.ceil( COMMAND_ORDER.length / 6.0 ) ) {
-                        sendHelpMessage( sender, page * 6, page * 6 + 6 );
-                    }
-
-                    else {
-                        sender.sendMessage( Utils.getColored( "&hInvalid page number!" ) );
-                    }
-                }
-
-                else {
-                    sendHelpMessage( sender, 0, 6 );
+                    CoreUtils.sendMsg( sender, "&sInvalid page number!" );
                 }
             }
 
             else {
                 sendHelpMessage( sender, 0, 6 );
+            }
+        }
+
+        else if ( args[0].equalsIgnoreCase( "reload" ) ) {
+            if ( VaultUtils.hasPerms( sender, Settings.RELOAD_PERMISSION.string() ) ) {
+                CoreUtils.sendMsg( sender, "&7Attempting to reload &6Netuno&7..." );
+                CoreUtils.logInfo( "Attempting to reload Netuno..." );
+
+                YMLUtils.getConfig().getYMLManager().initialize();
+
+                CoreUtils.sendMsg( sender, "&7Successfully reloaded &6Netuno" );
+                CoreUtils.logInfo( "Successfully reloaded Netuno and its files" );
+            }
+
+            else {
+                sendPermissionMsg( sender );
             }
         }
 
@@ -104,7 +87,7 @@ public class NetunoCmd extends BaseCommand {
     private void sendHelpMessage( CommandSender sender, int start, int end ) {
         String toSend = "\n";
         for ( int index = start; index < end; index++ ) {
-            toSend += CommandErrors.getCommandUsage( COMMAND_ORDER[index] ) + "\n";
+            toSend += Netuno.registeredCommands.get( index ).getUsage() + "\n";
         }
         toSend += "\n";
         Utils.sendAnyMsg( sender, toSend );
