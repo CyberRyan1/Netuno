34690be7b4233451715fa0d4d4b85699c6ddd15d
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-30 13:19:32 -0500

    Fully refactored to use CyberCore

--- src/main/java/com/github/cyberryan1/netuno/commands/Togglesigns.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/Togglesigns.java
@@ -1,103 +1,87 @@
 package com.github.cyberryan1.netuno.commands;
 
-import com.github.cyberryan1.cybercore.utils.VaultUtils;
-import com.github.cyberryan1.netuno.classes.BaseCommand;
-import com.github.cyberryan1.netuno.utils.CommandErrors;
+import com.github.cyberryan1.cybercore.helpers.command.CyberCommand;
+import com.github.cyberryan1.cybercore.utils.CoreUtils;
 import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.database.Database;
-import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
-import org.bukkit.command.Command;
+import com.github.cyberryan1.netuno.utils.settings.Settings;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Collections;
 import java.util.List;
 
-public class Togglesigns extends BaseCommand {
-
-    private final Database DATA = Utils.getDatabase();
+public class Togglesigns extends CyberCommand {
 
     public Togglesigns() {
-        super( "reports", YMLUtils.getConfig().getStr( "signs.notifs-perm" ), getColorizedStr( "&8/&ureports &y[player]" ) );
+        super(
+                "togglesigns",
+                Settings.SIGN_NOTIFS_PERMISSION.string(),
+                Settings.PERM_DENIED_MSG.string(),
+                "&8/&stogglesigns &p[enable/disable]"
+        );
+        register( true );
+
+        demandPermission( true );
+        demandPlayer( true );
+        setAsync( true );
     }
 
 
     @Override
-    public List<String> onTabComplete( CommandSender sender, Command command, String label, String[] args ) {
+    public List<String> tabComplete( CommandSender sender, String[] args ) {
         if ( permissionsAllowed( sender ) ) {
-            if ( args.length == 0 || args[0].length() == 0 ) {
-                return addAllToList( "enable", "disable" );
-            }
-            else if ( "ENABLE".startsWith( args[0].toUpperCase() ) ) {
-                return addAllToList( "enable" );
-            }
-            else if ( "DISABLE".startsWith( args[0].toUpperCase() ) ) {
-                return addAllToList( "disable" );
-            }
+            List<String> suggestions = List.of( "enable", "disable" );
+            if ( args.length == 0 || args[0].length() == 0 ) { return suggestions; }
+            else if ( args.length == 1 ) { return matchArgs( suggestions, args[0] ); }
         }
 
-        return Collections.emptyList();
+        return List.of();
     }
 
     @Override
     // /togglesigns
     // /togglesigns [enable/disable]
-    public boolean onCommand( CommandSender sender, Command command, String label, String args[] ) {
+    public boolean execute( CommandSender sender, String args[] ) {
+        final Player player = ( Player ) sender;
+        final boolean noSignNotifs = Utils.getDatabase().checkPlayerNoSignNotifs( player ); // true = no sign notifs is enabled
 
-        if ( VaultUtils.hasPerms( sender, YMLUtils.getConfig().getStr( "signs.notifs-perm" ) ) == false ) {
-            CommandErrors.sendInvalidPerms( sender );
-            return true;
-        }
-
-        if ( sender instanceof Player == false ) {
-            CommandErrors.sendCanOnlyBeRanByPlayer( sender );
-            return true;
-        }
-        Player player = ( Player ) sender;
-
-        // /togglesigns
-        if ( Utils.isOutOfBounds( args, 0 ) ) {
+        if ( args.length == 0 ) {
             // currently has sign notifs enabled
-            if ( DATA.checkPlayerNoSignNotifs( player ) == false ) {
-                DATA.addPlayerNoSignNotifs( player );
-                player.sendMessage( Utils.getColored( "&cDisabled&h sign notifications" ) );
+            if ( noSignNotifs == false ) {
+                Utils.getDatabase().addPlayerNoSignNotifs( player );
+                CoreUtils.sendMsg( player, "&cDisabled &ssign notifications" );
             }
 
             // currently has sign notifs disabled
             else {
-                DATA.removePlayerNoSignNotifs( player );
-                player.sendMessage( Utils.getColored( "&aEnabled&h sign notifications" ) );
+                Utils.getDatabase().removePlayerNoSignNotifs( player );
+                CoreUtils.sendMsg( player, "&aEnabled &ssign notifications" );
             }
         }
 
-        // /togglesigns [enable/disable]
-        else {
-            if ( args[0].equalsIgnoreCase( "enable" ) ) {
-                if ( DATA.checkPlayerNoSignNotifs( player ) ) {
-                    DATA.removePlayerNoSignNotifs( player );
-                    player.sendMessage( Utils.getColored( "&aEnabled&h sign notifications" ) );
-                }
-
-                else {
-                    player.sendMessage( Utils.getColored( "&hSign notifications are already &aenabled" ) );
-                }
-
-            }
-
-            else if ( args[0].equalsIgnoreCase( "disable" ) ) {
-                if ( DATA.checkPlayerNoSignNotifs( player ) == false ) {
-                    DATA.addPlayerNoSignNotifs( player );
-                    player.sendMessage( Utils.getColored( "&cDisabled&h sign notifications" ) );
-                }
-
-                else {
-                    player.sendMessage( Utils.getColored( "&hSign notifications are already &cdisabled" ) );
-                }
+        else if ( args[0].equalsIgnoreCase( "enable" ) ) {
+            if ( noSignNotifs == false ) {
+                CoreUtils.sendMsg( player, "&sSign notifications are already enabled" );
             }
 
             else {
-                CommandErrors.sendCommandUsage( player, "togglesigns" );
+                Utils.getDatabase().removePlayerNoSignNotifs( player );
+                CoreUtils.sendMsg( player, "&aEnabled &ssign notifications" );
             }
+        }
+
+        else if ( args[0].equalsIgnoreCase( "disable" ) ) {
+            if ( noSignNotifs ) {
+                CoreUtils.sendMsg( player, "&sSign notifications are already disabled" );
+            }
+
+            else {
+                Utils.getDatabase().addPlayerNoSignNotifs( player );
+                CoreUtils.sendMsg( player, "&cDisabled &ssign notifications" );
+            }
+        }
+
+        else {
+            sendUsage( player );
         }
 
         return true;
