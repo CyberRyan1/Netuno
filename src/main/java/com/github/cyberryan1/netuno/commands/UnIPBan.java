6050fd91d5b3512adf0ac3fde8cb7f95d2a70315
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-30 13:25:36 -0500

    Fully refactored to use CyberCore

--- src/main/java/com/github/cyberryan1/netuno/commands/UnIPBan.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/UnIPBan.java
@@ -1,100 +1,80 @@
 package com.github.cyberryan1.netuno.commands;
 
-import com.github.cyberryan1.cybercore.utils.VaultUtils;
-import com.github.cyberryan1.netuno.classes.BaseCommand;
+import com.github.cyberryan1.cybercore.helpers.command.ArgType;
+import com.github.cyberryan1.cybercore.helpers.command.CyberCommand;
 import com.github.cyberryan1.netuno.classes.IPPunishment;
 import com.github.cyberryan1.netuno.utils.CommandErrors;
 import com.github.cyberryan1.netuno.utils.Time;
 import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.database.Database;
-import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
+import com.github.cyberryan1.netuno.utils.settings.Settings;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class UnIPBan extends BaseCommand {
-
-    private Database DATA = Utils.getDatabase();
+public class UnIPBan extends CyberCommand {
 
     public UnIPBan() {
-        super( "unipban", YMLUtils.getConfig().getStr( "unipban.perm" ), getColorizedStr( "&8/&uunipban &y(player)" ) );
+        super(
+                "unipban",
+                Settings.UNIPBAN_PERMISSION.string(),
+                Settings.PERM_DENIED_MSG.string(),
+                "&8/&sunipban &p(player)"
+        );
+        register( true );
+
+        demandPermission( true );
+        setMinArgs( 1 );
+        setArgType( 0, ArgType.OFFLINE_PLAYER );
+        setAsync( true );
     }
 
     @Override
-    public List<String> onTabComplete( CommandSender sender, Command command, String label, String[] args ) {
-        return Collections.emptyList();
+    public List<String> tabComplete( CommandSender sender, String[] args ) {
+        return List.of();
     }
 
     @Override
-    public boolean onCommand( CommandSender sender, Command command, String label, String args[] ) {
+    public boolean execute( CommandSender sender, String args[] ) {
+        final OfflinePlayer target = Bukkit.getOfflinePlayer( args[0] );
 
-        if ( VaultUtils.hasPerms( sender, YMLUtils.getConfig().getStr( "unipban.perm" ) ) == false ) {
-            CommandErrors.sendInvalidPerms( sender );
-            return true;
+        List<OfflinePlayer> punishedAccounts = Utils.getDatabase().getPunishedAltsByType( target.getUniqueId().toString(), "ipban" );
+        List<IPPunishment> punishments = new ArrayList<>();
+        for ( OfflinePlayer account : punishedAccounts ) {
+            punishments.addAll( Utils.getDatabase().getIPPunishment( account.getUniqueId().toString(), "ipban", true ) );
         }
 
-        if ( Utils.isOutOfBounds( args, 0 ) == false ) {
-
-            if ( Utils.isValidUsername( args[0] ) == false ) {
-                CommandErrors.sendPlayerNotFound( sender, args[0] );
-                return true;
+        if ( punishments.size() >= 1 ) {
+            for ( IPPunishment pun : punishments ) {
+                Utils.getDatabase().setIPPunishmentActive( pun.getID(), false );
             }
 
-            OfflinePlayer target = Bukkit.getOfflinePlayer( args[0] );
-            if ( target != null ) {
+            IPPunishment pun = new IPPunishment();
+            pun.setPlayerUUID( target.getUniqueId().toString() );
+            pun.setReason( "" );
+            pun.setLength( -1L );
+            pun.setDate( Time.getCurrentTimestamp() );
+            pun.setType( "UnIPBan" );
+            pun.setActive( false );
 
-                List<OfflinePlayer> punishedAccounts = DATA.getPunishedAltsByType( target.getUniqueId().toString(), "ipban" );
-                List<IPPunishment> punishments = new ArrayList<>();
-                for ( OfflinePlayer account : punishedAccounts ) {
-                    punishments.addAll( DATA.getIPPunishment( account.getUniqueId().toString(), "ipban", true ) );
-                }
-
-                if ( punishments.size() >= 1 ) {
-                    for ( IPPunishment pun : punishments ) {
-                        DATA.setIPPunishmentActive( pun.getID(), false );
-                    }
-
-                    IPPunishment pun = new IPPunishment();
-                    pun.setPlayerUUID( target.getUniqueId().toString() );
-                    pun.setReason( "" );
-                    pun.setLength( -1L );
-                    pun.setDate( Time.getCurrentTimestamp() );
-                    pun.setType( "UnIPBan" );
-                    pun.setActive( false );
-
-                    pun.setStaffUUID( "CONSOLE" );
-                    if ( sender instanceof Player ) {
-                        Player staff = ( Player ) sender;
-                        pun.setStaffUUID( staff.getUniqueId().toString() );
-                    }
-
-                    DATA.addPunishment( pun );
-
-                    Utils.doPublicPunBroadcast( pun );
-                    Utils.doStaffPunBroadcast( pun );
-
-                }
-
-                else {
-                    CommandErrors.sendNoPunishments( sender, target.getName(), "ipban" );
-                }
-
+            pun.setStaffUUID( "CONSOLE" );
+            if ( sender instanceof Player ) {
+                Player staff = ( Player ) sender;
+                pun.setStaffUUID( staff.getUniqueId().toString() );
             }
 
-            else {
-                CommandErrors.sendPlayerNotFound( sender, args[0] );
-            }
+            Utils.getDatabase().addPunishment( pun );
+
+            Utils.doPublicPunBroadcast( pun );
+            Utils.doStaffPunBroadcast( pun );
 
         }
 
         else {
-            CommandErrors.sendCommandUsage( sender, "unipban" );
+            CommandErrors.sendNoPunishments( sender, target.getName(), "ipban" );
         }
 
         return true;
