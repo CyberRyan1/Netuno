abcb25afc8a2ce32450d38c061660e35cc469acb
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-30 12:23:05 -0500

    Fully refactored to use CyberCore

--- src/main/java/com/github/cyberryan1/netuno/commands/Kick.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/Kick.java
@@ -1,90 +1,56 @@
 package com.github.cyberryan1.netuno.commands;
 
-import com.github.cyberryan1.cybercore.utils.VaultUtils;
-import com.github.cyberryan1.netuno.classes.BaseCommand;
+import com.github.cyberryan1.cybercore.helpers.command.CyberCommand;
 import com.github.cyberryan1.netuno.classes.PrePunishment;
 import com.github.cyberryan1.netuno.utils.CommandErrors;
 import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.database.Database;
-import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
+import com.github.cyberryan1.netuno.utils.settings.Settings;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Collections;
 import java.util.List;
 
-public class Kick extends BaseCommand {
-
-    private final Database DATA = Utils.getDatabase();
+public class Kick extends CyberCommand {
 
     public Kick() {
-        super( "kick", YMLUtils.getConfig().getStr( "kick.perm" ), YMLUtils.getConfig().getColoredStr( "general.perm-denied-msg" ), getColorizedStr( "&8/&ukick &y(player) (reason) [-s]" ) );
+        super(
+                "kick",
+                Settings.KICK_PERMISSION.string(),
+                Settings.PERM_DENIED_MSG.string(),
+                "&8/&skick &p(player) (reason) [-s]"
+        );
     }
 
     @Override
-    public List<String> onTabComplete( CommandSender sender, Command command, String label, String[] args ) {
-        if ( permissionsAllowed( sender ) ) {
-            if ( args.length == 0 || args[0].length() == 0 ) {
-                return getAllOnlinePlayerNames();
-            }
-            else if ( args.length == 1 ) {
-                return matchOnlinePlayers( args[0] );
-            }
-        }
-        return Collections.emptyList();
+    public List<String> tabComplete( CommandSender sender, String[] args ) {
+        return List.of();
     }
 
     @Override
     // /kick (player) (reason)
-    public boolean onCommand( CommandSender sender, Command command, String label, String args[] ) {
-        if ( VaultUtils.hasPerms( sender, YMLUtils.getConfig().getStr( "kick.perm" ) ) ) {
-            if ( Utils.isOutOfBounds( args, 1 ) == false ) {
+    public boolean execute( CommandSender sender, String args[] ) {
+        final OfflinePlayer target = Bukkit.getOfflinePlayer( args[0] );
+        PrePunishment pun = new PrePunishment(
+                target,
+                "Kick",
+                Utils.getRemainingArgs( args, 1 )
+        );
 
-                if ( Utils.isValidUsername( args[0] ) == false ) {
-                    CommandErrors.sendPlayerNotFound( sender, args[0] );
-                    return true;
-                }
+        pun.setConsoleSender( true );
+        if ( sender instanceof Player ) {
+            Player staff = ( Player ) sender;
+            pun.setStaff( staff );
+            pun.setConsoleSender( false );
 
-                Player target = Bukkit.getServer().getPlayer( args[0] );
-                if ( target != null ) {
-                    PrePunishment pun = new PrePunishment(
-                            target,
-                            "Kick",
-                            Utils.getRemainingArgs( args, 1 )
-                    );
-
-                    pun.setConsoleSender( true );
-                    if ( sender instanceof Player ) {
-                        Player staff = ( Player ) sender;
-                        pun.setStaff( staff );
-                        pun.setConsoleSender( false );
-
-                        if ( Utils.checkStaffPunishmentAllowable( staff, target ) == false ) {
-                            CommandErrors.sendPlayerCannotBePunished( sender, target.getName() );
-                            return true;
-                        }
-                    }
-
-                    pun.executePunishment();
-                }
-
-                else {
-                    CommandErrors.sendPlayerNotFound( sender, args[0] );
-                }
-            }
-
-            else {
-                CommandErrors.sendCommandUsage( sender, "kick" );
+            if ( Utils.checkStaffPunishmentAllowable( staff, target ) == false ) {
+                CommandErrors.sendPlayerCannotBePunished( sender, target.getName() );
+                return true;
             }
         }
 
-        else {
-            CommandErrors.sendInvalidPerms( sender );
-        }
-
-
+        pun.executePunishment();
         return true;
     }
 }
