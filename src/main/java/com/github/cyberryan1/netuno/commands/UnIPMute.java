63dae741ced497603b88faaa16881363dc451d27
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-30 13:27:38 -0500

    Fully refactored to use CyberCore

--- src/main/java/com/github/cyberryan1/netuno/commands/UnIPMute.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/UnIPMute.java
@@ -1,120 +1,92 @@
 package com.github.cyberryan1.netuno.commands;
 
-import com.github.cyberryan1.cybercore.utils.VaultUtils;
-import com.github.cyberryan1.netuno.classes.BaseCommand;
+import com.github.cyberryan1.cybercore.helpers.command.ArgType;
+import com.github.cyberryan1.cybercore.helpers.command.CyberCommand;
 import com.github.cyberryan1.netuno.classes.IPPunishment;
 import com.github.cyberryan1.netuno.classes.Punishment;
 import com.github.cyberryan1.netuno.utils.CommandErrors;
 import com.github.cyberryan1.netuno.utils.Time;
 import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.database.Database;
-import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
+import com.github.cyberryan1.netuno.utils.settings.Settings;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class UnIPMute extends BaseCommand {
-
-    private final Database DATA = Utils.getDatabase();
+public class UnIPMute extends CyberCommand {
 
     public UnIPMute() {
-        super( "unipmute", YMLUtils.getConfig().getStr( "unipmute.perm" ), getColorizedStr( "&8/&uunipmute &y(player)" ) );
+        super(
+                "unipmute",
+                Settings.UNIPMUTE_PERMISSION.string(),
+                Settings.PERM_DENIED_MSG.string(),
+                "&8/&sunipmute &p(player)"
+        );
+        register( true );
+
+        demandPermission( true );
+        setMinArgs( 1 );
+        setArgType( 0, ArgType.OFFLINE_PLAYER );
+        setAsync( true );
     }
 
     @Override
-    public List<String> onTabComplete( CommandSender sender, Command command, String label, String[] args ) {
-        if ( permissionsAllowed( sender ) ) {
-            if ( args.length == 0 || args[0].length() == 0 ) {
-                return getAllOnlinePlayerNames();
-            }
-            else if ( args.length == 1 ) {
-                return matchOnlinePlayers( args[0] );
-            }
-        }
-        return Collections.emptyList();
+    public List<String> tabComplete( CommandSender sender, String[] args ) {
+        return List.of();
     }
 
     @Override
-    public boolean onCommand( CommandSender sender, Command command, String label, String args[] ) {
+    public boolean execute( CommandSender sender, String args[] ) {
+        final OfflinePlayer target = Bukkit.getOfflinePlayer( args[0] );
 
-        if ( VaultUtils.hasPerms( sender, YMLUtils.getConfig().getStr( "unipmute.perm" ) ) == false ) {
-            CommandErrors.sendInvalidPerms( sender );
-            return true;
+        List<OfflinePlayer> punishedAccounts = Utils.getDatabase().getPunishedAltsByType( target.getUniqueId().toString(), "ipmute" );
+        List<IPPunishment> punishments = new ArrayList<>();
+        for ( OfflinePlayer account : punishedAccounts ) {
+            punishments.addAll( Utils.getDatabase().getIPPunishment( account.getUniqueId().toString(), "ipmute", true ) );
         }
 
-        if ( Utils.isOutOfBounds( args, 0 ) == false ) {
-
-            if ( Utils.isValidUsername( args[0] ) == false ) {
-                CommandErrors.sendPlayerNotFound( sender, args[0] );
-                return true;
+        if ( punishments.size() >= 1 ) {
+            for ( IPPunishment pun : punishments ) {
+                Utils.getDatabase().setPunishmentActive( pun.getID(), false );
             }
 
-            OfflinePlayer target = Bukkit.getOfflinePlayer( args[0] );
-            if ( target != null ) {
+            Punishment pun = new Punishment();
+            pun.setPlayerUUID( target.getUniqueId().toString() );
+            pun.setReason( "" );
+            pun.setLength( -1L );
+            pun.setDate( Time.getCurrentTimestamp() );
+            pun.setType( "UnIPMute" );
+            pun.setActive( false );
 
-                List<OfflinePlayer> punishedAccounts = DATA.getPunishedAltsByType( target.getUniqueId().toString(), "ipmute" );
-                List<IPPunishment> punishments = new ArrayList<>();
-                for ( OfflinePlayer account : punishedAccounts ) {
-                    punishments.addAll( DATA.getIPPunishment( account.getUniqueId().toString(), "ipmute", true ) );
-                }
-
-                if ( punishments.size() >= 1 ) {
-                    for ( IPPunishment pun : punishments ) {
-                        DATA.setPunishmentActive( pun.getID(), false );
-                    }
-
-                    Punishment pun = new Punishment();
-                    pun.setPlayerUUID( target.getUniqueId().toString() );
-                    pun.setReason( "" );
-                    pun.setLength( -1L );
-                    pun.setDate( Time.getCurrentTimestamp() );
-                    pun.setType( "UnIPMute" );
-                    pun.setActive( false );
-
-                    pun.setStaffUUID( "CONSOLE" );
-                    if ( sender instanceof Player ) {
-                        Player staff = ( Player ) sender;
-                        pun.setStaffUUID( staff.getUniqueId().toString() );
-                    }
-
-                    DATA.addPunishment( pun );
-                    if ( target.isOnline() ) {
-                        Player targetOnline = target.getPlayer();
-                        Utils.sendPunishmentMsg( targetOnline, pun );
-                    }
-
-                    Utils.doPublicPunBroadcast( pun );
-                    Utils.doStaffPunBroadcast( pun );
-
-                    // Sends a notification to all online alts for the same punishment
-                    for ( OfflinePlayer alt : DATA.getAllAlts( target.getUniqueId().toString() ) ) {
-                        if ( alt.isOnline() && alt.getName().equals( target.getName() ) == false ) {
-                            pun.setPlayerUUID( alt.getUniqueId().toString() );
-                            Utils.sendPunishmentMsg( alt.getPlayer(), pun );
-                        }
-                    }
-                }
-
-                else {
-                    CommandErrors.sendNoPunishments( sender, target.getName(), "ipmute" );
-                }
-
+            pun.setStaffUUID( "CONSOLE" );
+            if ( sender instanceof Player ) {
+                Player staff = ( Player ) sender;
+                pun.setStaffUUID( staff.getUniqueId().toString() );
             }
 
-            else {
-                CommandErrors.sendPlayerNotFound( sender, args[0] );
+            Utils.getDatabase().addPunishment( pun );
+            if ( target.isOnline() ) {
+                Player targetOnline = target.getPlayer();
+                Utils.sendPunishmentMsg( targetOnline, pun );
             }
 
+            Utils.doPublicPunBroadcast( pun );
+            Utils.doStaffPunBroadcast( pun );
+
+            // Sends a notification to all online alts for the same punishment
+            for ( OfflinePlayer alt : Utils.getDatabase().getAllAlts( target.getUniqueId().toString() ) ) {
+                if ( alt.isOnline() && alt.getName().equals( target.getName() ) == false ) {
+                    pun.setPlayerUUID( alt.getUniqueId().toString() );
+                    Utils.sendPunishmentMsg( alt.getPlayer(), pun );
+                }
+            }
         }
 
         else {
-            CommandErrors.sendCommandUsage( sender, "unipmute" );
+            CommandErrors.sendNoPunishments( sender, target.getName(), "ipmute" );
         }
 
         return true;
