9e9053d2c53dfa519f832118f767920244fe4850
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-30 13:29:12 -0500

    Fully refactored to use CyberCore

--- src/main/java/com/github/cyberryan1/netuno/commands/Unmute.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/Unmute.java
@@ -1,105 +1,79 @@
 package com.github.cyberryan1.netuno.commands;
 
-import com.github.cyberryan1.cybercore.utils.VaultUtils;
-import com.github.cyberryan1.netuno.classes.BaseCommand;
+import com.github.cyberryan1.cybercore.helpers.command.ArgType;
+import com.github.cyberryan1.cybercore.helpers.command.CyberCommand;
 import com.github.cyberryan1.netuno.classes.Punishment;
 import com.github.cyberryan1.netuno.utils.CommandErrors;
 import com.github.cyberryan1.netuno.utils.Time;
 import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.database.Database;
-import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
+import com.github.cyberryan1.netuno.utils.settings.Settings;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class Unmute extends BaseCommand {
-
-    private final Database DATA = Utils.getDatabase();
+public class Unmute extends CyberCommand {
 
     public Unmute() {
-        super( "unmute", YMLUtils.getConfig().getStr( "unmute.perm" ), getColorizedStr( "&8/&uunmute &y(player)" ) );
+        super(
+                "unmute",
+                Settings.UNMUTE_PERMISSION.string(),
+                Settings.PERM_DENIED_MSG.string(),
+                "&8/&sunmute &p(player)"
+        );
+        register( true );
+
+        demandPermission( true );
+        setMinArgs( 1 );
+        setArgType( 0, ArgType.OFFLINE_PLAYER );
+        setAsync( true );
     }
 
     @Override
-    public List<String> onTabComplete( CommandSender sender, Command command, String label, String[] args ) {
-        if ( permissionsAllowed( sender ) ) {
-            if ( args.length == 0 || args[0].length() == 0 ) {
-                return getAllOnlinePlayerNames();
-            }
-            else if ( args.length == 1 ) {
-                return matchOnlinePlayers( args[0] );
-            }
-        }
-        return Collections.emptyList();
+    public List<String> tabComplete( CommandSender sender, String[] args ) {
+        return List.of();
     }
 
     @Override
     // /unmute (player)
-    public boolean onCommand( CommandSender sender, Command command, String label, String args[] ) {
+    public boolean execute( CommandSender sender, String args[] ) {
+        final OfflinePlayer target = Bukkit.getOfflinePlayer( args[0] );
 
-        if ( VaultUtils.hasPerms( sender, YMLUtils.getConfig().getStr( "unmute.perm" ) ) == false ) {
-            CommandErrors.sendInvalidPerms( sender );
-            return true;
-        }
-
-        if ( Utils.isOutOfBounds( args, 0 ) == false ) {
-
-            if ( Utils.isValidUsername( args[0] ) == false ) {
-                CommandErrors.sendPlayerNotFound( sender, args[0] );
-                return true;
+        ArrayList<Punishment> punishments = Utils.getDatabase().getPunishment( target.getUniqueId().toString(), "mute", true );
+        if ( punishments.size() >= 1 ) {
+            for ( Punishment pun : punishments ) {
+                Utils.getDatabase().setPunishmentActive( pun.getID(), false );
             }
 
-            OfflinePlayer target = Bukkit.getOfflinePlayer( args[0] );
-            if ( target != null ) {
-                ArrayList<Punishment> punishments = DATA.getPunishment( target.getUniqueId().toString(), "mute", true );
-                if ( punishments.size() >= 1 ) {
-                    for ( Punishment pun : punishments ) {
-                        DATA.setPunishmentActive( pun.getID(), false );
-                    }
+            Punishment unmutePun = new Punishment();
+            unmutePun.setPlayerUUID( target.getUniqueId().toString() );
+            unmutePun.setReason( "" );
+            unmutePun.setLength( -1L );
+            unmutePun.setDate( Time.getCurrentTimestamp() );
+            unmutePun.setType( "Unmute" );
+            unmutePun.setActive( false );
 
-                    Punishment unmutePun = new Punishment();
-                    unmutePun.setPlayerUUID( target.getUniqueId().toString() );
-                    unmutePun.setReason( "" );
-                    unmutePun.setLength( -1L );
-                    unmutePun.setDate( Time.getCurrentTimestamp() );
-                    unmutePun.setType( "Unmute" );
-                    unmutePun.setActive( false );
-
-                    unmutePun.setStaffUUID( "CONSOLE" );
-                    if ( sender instanceof Player ) {
-                        Player staff = ( Player ) sender;
-                        unmutePun.setStaffUUID( staff.getUniqueId().toString() );
-                    }
-
-                    DATA.addPunishment( unmutePun );
-                    if ( target.isOnline() ) {
-                        Player targetOnline = target.getPlayer();
-                        Utils.sendPunishmentMsg( targetOnline, unmutePun );
-                    }
-
-                    Utils.doPublicPunBroadcast( unmutePun );
-                    Utils.doStaffPunBroadcast( unmutePun );
-                }
-
-                else {
-                    CommandErrors.sendNoPunishments( sender, target.getName(), "mute" );
-                }
-
+            unmutePun.setStaffUUID( "CONSOLE" );
+            if ( sender instanceof Player ) {
+                Player staff = ( Player ) sender;
+                unmutePun.setStaffUUID( staff.getUniqueId().toString() );
             }
 
-            else {
-                CommandErrors.sendPlayerNotFound( sender, args[0] );
+            Utils.getDatabase().addPunishment( unmutePun );
+            if ( target.isOnline() ) {
+                Player targetOnline = target.getPlayer();
+                Utils.sendPunishmentMsg( targetOnline, unmutePun );
             }
+
+            Utils.doPublicPunBroadcast( unmutePun );
+            Utils.doStaffPunBroadcast( unmutePun );
         }
 
         else {
-            CommandErrors.sendCommandUsage( sender, "unmute" );
+            CommandErrors.sendNoPunishments( sender, target.getName(), "mute" );
         }
 
         return true;
