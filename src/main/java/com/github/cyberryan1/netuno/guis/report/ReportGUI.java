b93acf02ac7ce1c7e829782da685e1d06056b218
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-30 19:32:20 -0500

    Redid the ReportGUI

--- src/main/java/com/github/cyberryan1/netuno/guis/report/ReportGUI.java
+++ src/main/java/com/github/cyberryan1/netuno/guis/report/ReportGUI.java
@@ -1,201 +1,123 @@
 package com.github.cyberryan1.netuno.guis.report;
 
+import com.github.cyberryan1.cybercore.helpers.gui.GUI;
+import com.github.cyberryan1.cybercore.helpers.gui.GUIItem;
+import com.github.cyberryan1.cybercore.utils.CoreGUIUtils;
 import com.github.cyberryan1.cybercore.utils.CoreUtils;
 import com.github.cyberryan1.cybercore.utils.VaultUtils;
 import com.github.cyberryan1.netuno.classes.SingleReport;
-import com.github.cyberryan1.netuno.guis.events.GUIEventInterface;
-import com.github.cyberryan1.netuno.guis.events.GUIEventManager;
-import com.github.cyberryan1.netuno.guis.events.GUIEventType;
-import com.github.cyberryan1.netuno.guis.utils.GUIUtils;
 import com.github.cyberryan1.netuno.utils.CommandErrors;
 import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.database.Database;
-import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
+import com.github.cyberryan1.netuno.utils.settings.Settings;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
-import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReportGUI implements Listener {
 
-    private final Database DATA = Utils.getDatabase();
-
-    private final Inventory gui;
+    private final GUI gui;
     private final Player player;
     private final OfflinePlayer target;
-    private final ArrayList<String> reasons;
+    private final List<String> reasons;
+    private final int rowSize;
 
-    private int guiSize;
+    private List<String> selections = new ArrayList<>();
 
     public ReportGUI( Player player, OfflinePlayer target ) {
         this.player = player;
         this.target = target;
-        this.reasons = new ArrayList<>( List.of( YMLUtils.getConfig().getStrList( "report.reasons" ) ) );
-        //this.guiSize = 18 + ( ( int ) ( Math.ceil( reasons.size() / 6.0 ) ) * 9 );
+        this.reasons = Settings.REPORT_REASONS_LIST.arraylist();
+        this.rowSize = 4 + ( reasons.size() / 6 );
 
-        setGuiSize();
-        String guiName = CoreUtils.getColored( "&pReporting " + target.getName() );
-        gui = Bukkit.createInventory( null, guiSize, guiName );
+        String guiTitle = CoreUtils.getColored( "&sReporting &p" + target.getName() );
+        gui = new GUI( guiTitle, rowSize, CoreGUIUtils.getBackgroundGlass() );
         insertItems();
     }
 
-    private void setGuiSize() {
-        if ( reasons.size() <= 6 ) { this.guiSize = 36; }
-        else if ( reasons.size() <= 12 ) { this.guiSize = 45; }
-        else if ( reasons.size() <= 18 ) { this.guiSize = 54; }
-        else { this.guiSize = 63; }
-    }
-
     public void insertItems() {
-        // background glass: everywhere
         // reset selections: 11 || submit: 15
         // reports: start at 19-21 and 23-24, every row as needed
-        ItemStack items[] = new ItemStack[guiSize];
-        for ( int index = 0; index < items.length; index++ ) { items[index] = GUIUtils.getBackgroundGlass(); }
 
-        int reasonIndex = 0;
-        int guiIndex = 19;
-        for ( int row = 0; row < ( int ) Math.ceil( guiSize / 9.0 ); row++ ) {
-            for ( int col = 0; col < 6; col++ ) {
-                if ( reasonIndex >= reasons.size() ) { break; }
-                if ( reasons.get( reasonIndex ).equals( "" ) == false ) {
-                    items[guiIndex] = GUIUtils.setItemName( GUIUtils.getItemForVersion( "LIGHT_GRAY_WOOL", "WOOL", 8 ), "&7" + reasons.get( reasonIndex ) );
+        gui.setItem( 11, new GUIItem( Material.RED_WOOL, "&cReset Selections", 11, () -> {
+            player.closeInventory();
+            ReportGUI newGui = new ReportGUI( this.player, this.target );
+            newGui.open();
+            player.playSound( player.getLocation(), Sound.BLOCK_DISPENSER_FAIL, 10, 2 );
+        } ) );
+
+        gui.setItem( 15, new GUIItem( Material.GREEN_WOOL, "&aSubmit Report", 15, () -> {
+            player.closeInventory();
+            player.playSound( player.getLocation(), Sound.BLOCK_DISPENSER_FAIL, 10, 2 );
+
+            if ( selections.isEmpty() ) {
+                CommandErrors.sendReportNeedsOneReason( player );
+                return;
+            }
+
+            String reasonsList[] = selections.toArray( new String[selections.size()] );
+            String reasons = Utils.formatListIntoAmountString( reasonsList );
+
+            String sentMsg = Utils.getCombinedString( Settings.REPORT_CONFIRM_MESSAGE.coloredStringlist() );
+            sentMsg = sentMsg.replace( "[TARGET]", target.getName() ).replace( "[REASON]", reasons );
+            if ( sentMsg.replace( "\n", "" ).isBlank() == false ) { CoreUtils.sendMsg( player, sentMsg ); }
+
+            String staffMsg = Utils.getCombinedString( Settings.REPORT_STAFF_MESSAGE.coloredStringlist() );
+            staffMsg = staffMsg.replace( "[TARGET]", target.getName() )
+                    .replace( "[REASON]", reasons )
+                    .replace( "[PLAYER]", player.getName() );
+            boolean endsWithNewLine = staffMsg.endsWith( "\n" );
+            if ( staffMsg.replace( "\n", "" ).isBlank() == false ) {
+                for ( Player p : Bukkit.getOnlinePlayers() ) {
+                    if ( VaultUtils.hasPerms( p, Settings.STAFF_PERMISSION.string() ) ) {
+                        CoreUtils.sendMsg( p, staffMsg );
+                        if ( endsWithNewLine ) CoreUtils.sendMsg( p, "" ); // Unfortunately had to "hard-code" this one
+                    }
                 }
+            }
 
-                if ( guiIndex % 9 == 3 ) { guiIndex++; }
-                guiIndex++; reasonIndex++;
+            for ( String reason : reasonsList ) {
+                Utils.getDatabase().addReport( new SingleReport( this.target, this.player, reason ) );
+            }
+        } ) );
+
+        int guiIndex = 19;
+        int reasonsIndex = 0;
+        for ( int row = 0; row < ( 1 + ( reasons.size() / 6 ) ); row++ ) {
+            for ( int col = 0; col < 6; col++ ) {
+                if ( reasonsIndex >= reasons.size() ) { break; }
+
+                final int finalReasonIndex = reasonsIndex;
+                final int finalGuiIndex = guiIndex;
+                gui.setItem( guiIndex, new GUIItem( Material.LIGHT_GRAY_WOOL, "&7" + reasons.get( reasonsIndex ), guiIndex, () -> {
+                    selections.add( reasons.get( finalReasonIndex ) );
+                    player.playSound( player.getLocation(), Sound.BLOCK_DISPENSER_FAIL, 10, 2 );
+                    gui.updateItem( finalGuiIndex, new GUIItem( Material.LIME_WOOL, "&a" + reasons.get( finalReasonIndex ), finalGuiIndex ) );
+                } ) );
+
+                guiIndex++;
+                if ( col == 2 ) { guiIndex++; }
+                reasonsIndex++;
             }
 
             guiIndex += 2;
         }
 
-        items[11] = GUIUtils.setItemName( GUIUtils.getItemForVersion( "RED_WOOL", "WOOL", 14 ), "&cReset Selections" );
-        items[15] = GUIUtils.setItemName( GUIUtils.getItemForVersion( "GREEN_WOOL", "WOOL", 13 ), "&2Submit Report" );
-
-        gui.setContents( items );
+        gui.createInventory();
     }
 
-    public void openInventory( Player player ) {
+    public void open() {
         if ( reasons.size() > 18 ) {
             CommandErrors.sendConfigError( player );
             Utils.logError( "CONFIG ERROR >> The list \"report.reasons\" is greater than the limit, 18. Reports will not work until this is fixed." );
         }
         else {
-            player.openInventory( gui );
-            GUIEventManager.addEvent( this );
+            gui.openInventory( player );
         }
-    }
-
-    @GUIEventInterface( type = GUIEventType.INVENTORY_CLICK )
-    public void onInventoryClick( InventoryClickEvent event ) {
-        if ( player.getName().equals( event.getWhoClicked().getName() ) == false ) { return; }
-        if ( event.getView().getTitle().equals( CoreUtils.getColored( "&pReporting " + target.getName() ) ) == false ) { return; }
-
-        event.setCancelled( true );
-        if ( event.getClickedInventory() == null || event.getClickedInventory().getType() == InventoryType.PLAYER ) { return; }
-
-        ItemStack item = event.getCurrentItem();
-        if ( item == null || item.getType() == Material.AIR ) { return; }
-
-        if ( GUIUtils.checkItemEqualsForVersion( item, "LIGHT_GRAY_WOOL", "WOOL", 8 ) ) {
-            ItemStack newItem = GUIUtils.getItemForVersion( "LIME_WOOL", "WOOL", 5 );
-            ItemMeta newItemMeta = newItem.getItemMeta();
-            String newName = event.getCurrentItem().getItemMeta().getDisplayName();
-            newName = newName.replaceAll( CoreUtils.getColored( "&7" ), CoreUtils.getColored( "&a" ) );
-            newItemMeta.setDisplayName( newName );
-            newItem.setItemMeta( newItemMeta );
-
-            gui.setItem( event.getSlot(), newItem );
-            player.playSound( player.getLocation(), GUIUtils.getSoundForVersion( "BLOCK_DISPENSER_FAIL", "NOTE_PLING" ), 10, 2 );
-        }
-
-        else if ( GUIUtils.checkItemEqualsForVersion( item, "LIME_WOOL", "WOOL", 5 ) ) {
-
-            ItemStack newItem = GUIUtils.getItemForVersion( "LIGHT_GRAY_WOOL", "WOOL", 8 );
-            ItemMeta newItemMeta = newItem.getItemMeta();
-            String newName = event.getCurrentItem().getItemMeta().getDisplayName();
-            newName = newName.replaceAll( CoreUtils.getColored( "&a" ), CoreUtils.getColored( "&7" ) );
-            newItemMeta.setDisplayName( newName );
-            newItem.setItemMeta( newItemMeta );
-
-            gui.setItem( event.getSlot(), newItem );
-            player.playSound( player.getLocation(), GUIUtils.getSoundForVersion( "BLOCK_DISPENSER_FAIL", "NOTE_PLING" ), 10, 2 );
-        }
-
-        else if ( GUIUtils.checkItemEqualsForVersion( item, "RED_WOOL", "WOOL", 14 ) ) {
-            insertItems();
-            openInventory( player );
-            player.playSound( player.getLocation(), GUIUtils.getSoundForVersion( "BLOCK_DISPENSER_FAIL", "NOTE_PLING" ), 10, 2 );
-        }
-
-        else if ( GUIUtils.checkItemEqualsForVersion( item,"GREEN_WOOL", "WOOL", 13 ) ) {
-            player.closeInventory();
-            player.playSound( player.getLocation(), GUIUtils.getSoundForVersion( "BLOCK_DISPENSER_FAIL", "NOTE_PLING" ), 10, 2 );
-
-            ArrayList<SingleReport> reports = new ArrayList<>();
-            for ( ItemStack i : event.getClickedInventory().getContents() ) {
-                if ( GUIUtils.checkItemEqualsForVersion( i, "LIME_WOOL", "WOOL", 5 ) ) {
-                    SingleReport sr = new SingleReport();
-                    sr.setReporter( player );
-                    sr.setTarget( target );
-                    sr.setReason( i.getItemMeta().getDisplayName().replace( CoreUtils.getColored( "&a" ), "") );
-                    reports.add( sr );
-                }
-            }
-
-            if ( reports.size() == 0 ) {
-                CommandErrors.sendReportNeedsOneReason( player );
-                return;
-            }
-
-            String reasonText[] = new String[ reports.size() ];
-            for ( int index = 0; index < reasonText.length; index++ ) {
-                reasonText[index] = reports.get( index ).getReason();
-            }
-
-            String msg = Utils.getCombinedString( YMLUtils.getConfig().getColoredStrList( "report.confirm-msg" ) );
-            msg = msg.replace( "[TARGET]", target.getName() ).replace( "[REASON]", Utils.formatListIntoAmountString( reasonText ) );
-            if ( msg.equals( "" ) == false ) { Utils.sendAnyMsg( player, msg ); }
-
-            msg = Utils.getCombinedString( YMLUtils.getConfig().getColoredStrList( "report.staff-msg" ) ).replace( "[PLAYER]", player.getName() );
-            msg = msg.replace( "[TARGET]", target.getName() ).replace( "[REASON]", Utils.formatListIntoAmountString( reasonText ) );
-            if ( msg.equals( "" ) == false ) {
-                for ( Player p : Bukkit.getOnlinePlayers() ) {
-                    if ( VaultUtils.hasPerms( p, YMLUtils.getConfig().getStr( "general.staff-perm" ) ) ) {
-                        Utils.sendAnyMsg( p, msg );
-                    }
-                }
-            }
-
-            for ( SingleReport sr : reports ) {
-                DATA.addReport( sr );
-            }
-        }
-    }
-
-    @GUIEventInterface( type = GUIEventType.INVENTORY_DRAG )
-    public void onInventoryDrag( InventoryDragEvent event ) {
-        if ( player.getName().equals( event.getWhoClicked().getName() ) == false ) { return; }
-        if ( event.getView().getTitle().equals( CoreUtils.getColored( "&pReporting " + target.getName() ) ) == false ) { return; }
-        event.setCancelled( true );
-    }
-
-    @GUIEventInterface( type = GUIEventType.INVENTORY_CLOSE )
-    public void onInventoryClose( InventoryCloseEvent event ) {
-        if ( player.getName().equals( event.getPlayer().getName() ) == false ) { return; }
-        if ( event.getView().getTitle().equals( CoreUtils.getColored( "&pReporting " + target.getName() ) ) == false ) { return; }
-
-        GUIEventManager.removeEvent( this );
     }
 }
