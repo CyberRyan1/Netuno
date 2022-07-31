741ec503571bc20a06066a22014fc8b21e8c79de
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-30 19:31:59 -0500

    Redid the StaffAllReportsGUI

--- src/main/java/com/github/cyberryan1/netuno/guis/report/StaffAllReportsGUI.java
+++ src/main/java/com/github/cyberryan1/netuno/guis/report/StaffAllReportsGUI.java
@@ -1,26 +1,18 @@
 package com.github.cyberryan1.netuno.guis.report;
 
-import com.github.cyberryan1.cybercore.utils.CoreUtils;
+import com.github.cyberryan1.cybercore.CyberCore;
+import com.github.cyberryan1.cybercore.helpers.gui.GUI;
+import com.github.cyberryan1.cybercore.helpers.gui.GUIItem;
+import com.github.cyberryan1.cybercore.utils.CoreGUIUtils;
 import com.github.cyberryan1.netuno.classes.CombinedReport;
 import com.github.cyberryan1.netuno.classes.SingleReport;
-import com.github.cyberryan1.netuno.guis.events.GUIEventInterface;
-import com.github.cyberryan1.netuno.guis.events.GUIEventManager;
-import com.github.cyberryan1.netuno.guis.events.GUIEventType;
-import com.github.cyberryan1.netuno.guis.utils.GUIUtils;
 import com.github.cyberryan1.netuno.guis.utils.SortBy;
-import com.github.cyberryan1.netuno.utils.CommandErrors;
 import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.database.Database;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.OfflinePlayer;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
-import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
@@ -30,29 +22,26 @@ import java.util.stream.Collectors;
 
 public class StaffAllReportsGUI implements Listener {
 
-    private final Database DATA = Utils.getDatabase();
-
-    private final Inventory gui;
+    private final GUI gui;
     private final Player staff;
     private final int page;
+    private final SortBy sort;
 
-    private SortBy sort;
-    private ArrayList<SingleReport> reports;
+    private List<SingleReport> allSingularReports;
     private List<CombinedReport> combinedReports = new ArrayList<>();
-    private int totalCombinedReportCount = 0;
 
     public StaffAllReportsGUI( Player staff, int page, SortBy sort ) {
         this.staff = staff;
         this.page = page;
         this.sort = sort;
 
-        DATA.deleteAllExpiredReports();
-        reports = DATA.getAllReports(); // ? very laggy, will need to fix in the future
+        Utils.getDatabase().deleteAllExpiredReports();
+        allSingularReports = Utils.getDatabase().getAllReports();
+
         compressReports();
         sort();
 
-        String guiName = CoreUtils.getColored( "&pReports" );
-        gui = Bukkit.createInventory( null, 54, guiName );
+        gui = new GUI( "&pReports", 6, CoreGUIUtils.getBackgroundGlass() );
         insertItems();
     }
 
@@ -60,19 +49,102 @@ public class StaffAllReportsGUI implements Listener {
         this( staff, page, SortBy.ONLINE );
     }
 
+    public void insertItems() {
+        // light background glass: 10-16, 19-25, 28-34
+        // reports: 10-16, 19-25, 28-34
+        // sort hopper: 40
+        // back book: 47 || next book: 51
+
+        int reportIndex = 0;
+        int guiIndex = 10;
+        for ( int row = 0; row < 3; row++ ) {
+            for ( int col = 0; col < 7; col++ ) {
+                if ( reportIndex < combinedReports.size() ) {
+                    final int reportIndexFinal = reportIndex;
+                    gui.setItem( guiIndex, new GUIItem( combinedReports.get( reportIndex ).getAsItem(), guiIndex, () -> {
+                        staff.closeInventory();
+                        staff.playSound( staff.getLocation(), Sound.BLOCK_DISPENSER_FAIL, 10, 2 );
+                        StaffPlayerReportsGUI playerReports = new StaffPlayerReportsGUI( staff, combinedReports.get( reportIndexFinal ).getTarget() );
+                        playerReports.open();
+                    } ) );
+                }
+
+                else {
+                    gui.setItem( guiIndex, new GUIItem( Material.LIGHT_GRAY_STAINED_GLASS_PANE, "&7", guiIndex ) );
+                }
+
+                guiIndex++;
+                reportIndex++;
+            }
+
+            guiIndex += 2;
+        }
+
+        gui.setItem( 40, new GUIItem( getSortHopper(), 40, () -> {
+            staff.playSound( staff.getLocation(), Sound.ITEM_BOOK_PAGE_TURN, 10, 1 );
+            SortBy newSort = switch ( sort ) {
+                case FIRST_DATE -> SortBy.LAST_DATE;
+                case LAST_DATE -> SortBy.ONLINE;
+                case ONLINE -> SortBy.OFFLINE;
+                default -> SortBy.FIRST_DATE;
+            };
+
+            StaffAllReportsGUI newGUI = new StaffAllReportsGUI( staff, page, newSort );
+            newGUI.open();
+        } ) );
+
+        if ( page >= 2 ) {
+            gui.setItem( 47, new GUIItem( Material.BOOK, "&pPrevious Page", 47, () -> {
+                staff.playSound( staff.getLocation(), Sound.ITEM_BOOK_PAGE_TURN, 10, 1 );
+                StaffAllReportsGUI newGUI = new StaffAllReportsGUI( staff, page - 1, sort );
+                newGUI.open();
+            } ) );
+        }
+
+        int maxPage = ( int ) Math.ceil( combinedReports.size() / 21.0 );
+        if ( page < maxPage ) {
+            gui.setItem( 51, new GUIItem( Material.BOOK, "&pNext Page", 51, () -> {
+                staff.playSound( staff.getLocation(), Sound.ITEM_BOOK_PAGE_TURN, 10, 1 );
+                StaffAllReportsGUI newGUI = new StaffAllReportsGUI( staff, page + 1, sort );
+                newGUI.open();
+            } ) );
+        }
+
+        gui.createInventory();
+    }
+
+    public void open() {
+        Bukkit.getScheduler().runTask( CyberCore.getPlugin(), () -> {
+            gui.openInventory( staff );
+        } );
+    }
+
+    private ItemStack getSortHopper() {
+        ItemStack hopper = CoreGUIUtils.createItem( Material.HOPPER, "&sPage &p#" + page );
+        if ( sort == SortBy.FIRST_DATE ) { return CoreGUIUtils.setItemLore( hopper, "", "&sSort: &pFirst Created -> Last Created",
+                "&sNext Sort: &pLast Created -> First Created", "&sClick to change sort method" ); }
+        else if ( sort == SortBy.LAST_DATE ) { return CoreGUIUtils.setItemLore( hopper, "", "&sSort: &pLast Created -> First Created",
+                "&sNext Sort: &pOnline -> Offline", "&sClick to change sort method" ); }
+        else if ( sort == SortBy.ONLINE ) { return CoreGUIUtils.setItemLore( hopper, "", "&sSort: &pOnline -> Offline",
+                "&sNext Sort: &pOffline -> Online", "&sClick to change sort method" ); }
+        else if ( sort == SortBy.OFFLINE ) { return CoreGUIUtils.setItemLore( hopper, "", "&sSort: &pOffline -> Online",
+                "&sNext Sort: &pFirst Created -> Last Created", "&sClick to change sort method" ); }
+
+        return null;
+    }
+
     private void compressReports() {
-        if ( reports.size() == 0 ) { return; }
+        if ( allSingularReports.size() == 0 ) { return; }
 
         ArrayList<UUID> playersReported = new ArrayList<>();
-        for ( int index = reports.size() - 1; index >= 0; index-- ) {
-            UUID uuid = reports.get( index ).getTarget().getUniqueId();
+        for ( int index = allSingularReports.size() - 1; index >= 0; index-- ) {
+            UUID uuid = allSingularReports.get( index ).getTarget().getUniqueId();
             if ( playersReported.contains( uuid ) == false ) { playersReported.add( uuid ); }
         }
 
         for ( UUID uuid : playersReported ) {
             combinedReports.add( new CombinedReport( Bukkit.getOfflinePlayer( uuid ) ) );
         }
-        totalCombinedReportCount = combinedReports.size();
 
         ArrayList<CombinedReport> tempReports = new ArrayList<>();
         final int START = 21 * ( page - 1 );
@@ -85,7 +157,7 @@ public class StaffAllReportsGUI implements Listener {
     }
 
     private void sort() {
-        if ( reports.size() == 0 ) { return; }
+        if ( allSingularReports.size() == 0 ) { return; }
         if ( sort == SortBy.ONLINE || sort == SortBy.OFFLINE ) {
             combinedReports = combinedReports.stream()
                     .sorted( ( r1, r2 ) -> (
@@ -108,124 +180,5 @@ public class StaffAllReportsGUI implements Listener {
                     ) )
                     .collect( Collectors.toList() );
         }
-    }
-
-    public void insertItems() {
-        // dark background glass: everywhere
-        // light background glass: 10-16, 19-25, 28-34
-        // reports: 10-16, 19-25, 28-34
-        // back book: 47 || next book: 51
-        // paper: 40
-        ItemStack items[] = new ItemStack[54];
-        for ( int index = 0; index < items.length; index++ ) {
-            items[index] = GUIUtils.getBackgroundGlass();
-        }
-
-        int reportIndex = 0;
-        int guiIndex = 10;
-        for ( int row = 0; row < 3; row++ ) {
-            for ( int col = 0; col < 7; col++ ) {
-                if ( reportIndex >= combinedReports.size() ) { items[guiIndex] = GUIUtils.getBackgroundGlass(); }
-                else { items[guiIndex] = combinedReports.get( reportIndex ).getAsItem(); }
-
-                guiIndex++; reportIndex++;
-            }
-
-            guiIndex += 2;
-        }
-
-        items[40] = getPaper();
-
-        if ( page >= 2 ) { items[47] = GUIUtils.createItem( Material.BOOK, "&pPrevious Page" ); }
-        int maxPage = ( int ) Math.ceil( totalCombinedReportCount / 21.0 );
-        if ( page < maxPage ) { items[51] = GUIUtils.createItem( Material.BOOK, "&pNext Page" ); }
-
-        gui.setContents( items );
-    }
-
-    private ItemStack getPaper() {
-        ItemStack paper = GUIUtils.createItem( Material.PAPER, "&sPage &p#" + page );
-        if ( sort == SortBy.FIRST_DATE ) { return GUIUtils.setItemLore( paper, "", "&sSort: &pFirst Created -> Last Created",
-                    "&sNext Sort: &pLast Created -> First Created", "&sClick to change sort method" ); }
-        else if ( sort == SortBy.LAST_DATE ) { return GUIUtils.setItemLore( paper, "", "&sSort: &pLast Created -> First Created",
-                "&sNext Sort: &pOnline -> Offline", "&sClick to change sort method" ); }
-        else if ( sort == SortBy.ONLINE ) { return GUIUtils.setItemLore( paper, "", "&sSort: &pOnline -> Offline",
-                "&sNext Sort: &pOffline -> Online", "&sClick to change sort method" ); }
-        else if ( sort == SortBy.OFFLINE ) { return GUIUtils.setItemLore( paper, "", "&sSort: &pOffline -> Online",
-                "&sNext Sort: &pFirst Created -> Last Created", "&sClick to change sort method" ); }
-
-        return null;
-    }
-
-    public void openInventory( Player staff ) {
-        staff.openInventory( gui );
-        GUIEventManager.addEvent( this );
-    }
-
-    @GUIEventInterface( type = GUIEventType.INVENTORY_CLICK )
-    public void onInventoryClick( InventoryClickEvent event ) {
-        if ( staff.getName().equals( event.getWhoClicked().getName() ) == false ) { return; }
-        if ( event.getView().getTitle().equals( CoreUtils.getColored( "&pReports" ) ) == false ) { return; }
-
-        event.setCancelled( true );
-        if ( event.getClickedInventory() == null || event.getClickedInventory().getType() == InventoryType.PLAYER ) { return; }
-
-        ItemStack clicked = event.getCurrentItem();
-        if ( clicked == null || clicked.getType() == Material.AIR ) { return; }
-
-        if ( clicked.equals( GUIUtils.createItem( Material.BOOK, "&pPrevious Page" ) ) ) {
-            StaffAllReportsGUI newGUI = new StaffAllReportsGUI( staff, page - 1, sort );
-            newGUI.openInventory( staff );
-            staff.playSound( staff.getLocation(), GUIUtils.getSoundForVersion( "BLOCK_DISPENSER_FAIL", "NOTE_PLING" ), 10, 2 );
-        }
-
-        else if ( clicked.equals( GUIUtils.createItem( Material.BOOK, "&pNext Page" ) ) ) {
-            StaffAllReportsGUI newGUI = new StaffAllReportsGUI( staff, page + 1, sort );
-            newGUI.openInventory( staff );
-            staff.playSound( staff.getLocation(), GUIUtils.getSoundForVersion( "BLOCK_DISPENSER_FAIL", "NOTE_PLING" ), 10, 2 );
-        }
-
-        else if ( clicked.equals( getPaper() ) ) {
-            StaffAllReportsGUI newGUI;
-            if ( sort == SortBy.FIRST_DATE ) { newGUI = new StaffAllReportsGUI( staff, page, SortBy.LAST_DATE ); }
-            else if ( sort == SortBy.LAST_DATE ) { newGUI = new StaffAllReportsGUI( staff, page, SortBy.ONLINE ); }
-            else if ( sort == SortBy.ONLINE ) { newGUI = new StaffAllReportsGUI( staff, page, SortBy.OFFLINE ); }
-            else { newGUI = new StaffAllReportsGUI( staff, page, SortBy.FIRST_DATE ); }
-
-            newGUI.openInventory( staff );
-            staff.playSound( staff.getLocation(), GUIUtils.getSoundForVersion( "ITEM_BOOK_PAGE_TURN", "NOTE_PLING" ), 10, 1 );
-        }
-
-        else if ( clicked.getType().equals( GUIUtils.getItemForVersion( "PLAYER_HEAD", "SKULL_ITEM" ).getType() ) ) {
-            String itemName = clicked.getItemMeta().getDisplayName();
-            itemName = Utils.removeColorCodes( itemName );
-            OfflinePlayer target = Bukkit.getOfflinePlayer( itemName );
-
-            if ( target != null ) {
-                StaffPlayerReportsGUI newGui = new StaffPlayerReportsGUI( staff, target );
-                staff.closeInventory();
-                staff.playSound( staff.getLocation(), GUIUtils.getSoundForVersion( "BLOCK_DISPENSER_FAIL", "NOTE_PLING" ), 10, 2 );
-                newGui.openInventory( staff );
-            }
-
-            else { // just in case
-                CommandErrors.sendPlayerNotFound( staff, itemName );
-            }
-        }
-    }
-
-    @GUIEventInterface( type = GUIEventType.INVENTORY_DRAG )
-    public void onInventoryDrag( InventoryDragEvent event ) {
-        if ( staff.getName().equals( event.getWhoClicked().getName() ) == false ) { return; }
-        if ( event.getView().getTitle().equals( CoreUtils.getColored( "&pReports" ) ) == false ) { return; }
-        event.setCancelled( true );
-    }
-
-    @GUIEventInterface( type = GUIEventType.INVENTORY_CLOSE )
-    public void onInventoryClose( InventoryCloseEvent event ) {
-        if ( staff.getName().equals( event.getPlayer().getName() ) == false ) { return; }
-        if ( event.getView().getTitle().equals( CoreUtils.getColored( "&pReports" ) ) == false ) { return; }
-
-        GUIEventManager.removeEvent( this );
     }
 }
