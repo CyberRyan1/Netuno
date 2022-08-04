d8ded5fe1a581ce0cadc9074c190e9a63eca9674
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-03 21:41:57 -0500

    Added many more methods

--- src/main/java/com/github/cyberryan1/netuno/api/ApiNetuno.java
+++ src/main/java/com/github/cyberryan1/netuno/api/ApiNetuno.java
@@ -1,19 +1,73 @@
 package com.github.cyberryan1.netuno.api;
 
 import com.github.cyberryan1.netuno.api.database.ConnectionManager;
-import com.github.cyberryan1.netuno.api.database.DatabaseManager;
+import com.github.cyberryan1.netuno.utils.settings.Settings;
 import com.github.cyberryan1.netunoapi.NetunoApi;
 import com.github.cyberryan1.netunoapi.database.DatabaseConnection;
 import com.github.cyberryan1.netunoapi.database.NetunoDatabases;
 
 public class ApiNetuno implements NetunoApi {
 
-    private DatabaseConnection connection = new ConnectionManager();
-    private NetunoDatabases databases = new DatabaseManager();
+    //
+    // Static Methods
+    //
+
+    private static ApiNetuno instance;
+
+    public static void setupInstance() {
+        instance = new ApiNetuno();
+
+        ConnectionManager conn = new ConnectionManager();
+        // Initialize the database connection
+        if ( Settings.DATABASE_USE_SQLITE.bool() ) {
+            conn.initializeSqlite();
+        }
+        else {
+            conn.initializeSql(
+                    Settings.DATABASE_SQL_HOST.string(),
+                    Settings.DATABASE_SQL_PORT.integer(),
+                    Settings.DATABASE_SQL_DATABASE.string(),
+                    Settings.DATABASE_SQL_USERNAME.string(),
+                    Settings.DATABASE_SQL_PASSWORD.string()
+            );
+        }
+        instance.setConnection( conn );
+
+        // Initialize the punishment and report databases' cache times
+        if ( Settings.CACHE_EXPIRATION.integer() > 0 ) {
+            instance.getDatabases().getPun().getCache().setExpirationTime( Settings.CACHE_EXPIRATION.integer() );
+            instance.getDatabases().getReports().getCache().setExpirationTime( Settings.CACHE_EXPIRATION.integer() );
+        }
+    }
+
+    public static void deleteInstance() {
+        ( ( ConnectionManager ) instance.getConnectionManager() ).closeConnection();
+
+        instance = null;
+    }
+
+    public static ApiNetuno getInstance() {
+        return instance;
+    }
+
+    public static NetunoDatabases getData() {
+        return instance.getDatabaseManager();
+    }
+
+    //
+    // Class Variables & Methods
+    //
+
+    private DatabaseConnection connection = null;
+    private NetunoDatabases databases = null;
 
     @Override
     public DatabaseConnection getConnectionManager() {
         return connection;
+    }
+
+    public void setConnection( ConnectionManager connection ) {
+        this.connection = connection;
     }
 
     @Override
