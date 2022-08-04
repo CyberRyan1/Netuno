105301543f838c2d5c1ce3962369a01178766865
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-03 23:29:51 -0500

    Refactored to use NetunoApi

--- src/main/java/com/github/cyberryan1/netuno/managers/ChatslowManager.java
+++ src/main/java/com/github/cyberryan1/netuno/managers/ChatslowManager.java
@@ -1,34 +1,67 @@
 package com.github.cyberryan1.netuno.managers;
 
-import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.database.Database;
-import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
+import com.github.cyberryan1.cybercore.CyberCore;
+import com.github.cyberryan1.netuno.api.ApiNetuno;
+import com.github.cyberryan1.netuno.utils.settings.Settings;
+import org.bukkit.Bukkit;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ChatslowManager {
 
-    private static final Database DATA = Utils.getDatabase();
     private static final String DATA_KEY = "chatslow-amount";
 
     private static int slow;
+    private static String TABLE_NAME;
 
     public ChatslowManager() {
-        if ( DATA.otherCheckKeyExists( DATA_KEY ) == false ) {
-            slow = YMLUtils.getConfig().getInt( "chatslow.default-value" );
-            DATA.addOther( DATA_KEY, slow + "" );
-        }
-        else {
-            setSlow( Integer.parseInt( DATA.getOther( DATA_KEY ) ) );
-        }
-    }
+        Bukkit.getScheduler().runTaskAsynchronously( CyberCore.getPlugin(), () -> {
+            try {
+                PreparedStatement ps = ApiNetuno.getInstance().getConnectionManager()
+                        .getConn().prepareStatement( "INSERT INTO random(key, value) VALUES(?, ?) ON DUPLICATE KEY UPDATE key = ?;" );
+                ps.setString( 1, DATA_KEY );
+                ps.setString( 2, Settings.CHATSLOW_DEFAULT_VALUE.integer() + "" );
+                ps.setString( 3, DATA_KEY );
 
-    public static void updateDatabase() {
-        DATA.updateOther( DATA_KEY, "" + slow );
+                ps.addBatch();
+                ps.executeBatch();
+                ps.close();
+
+                ps = ApiNetuno.getInstance().getConnectionManager()
+                        .getConn().prepareStatement( "SELECT * FROM random WHERE key = ?;" );
+                ps.setString( 1, DATA_KEY );
+
+                ResultSet rs = ps.executeQuery();
+                if ( rs.next() ) {
+                    slow = Integer.parseInt( rs.getString( "value" ) );
+                }
+
+                ps.close();
+                rs.close();
+            } catch ( SQLException e ) {
+                throw new RuntimeException( e );
+            }
+        } );
     }
 
     public static int getSlow() { return slow; }
 
     public static void setSlow( int newSlow ) {
         slow = newSlow;
-        updateDatabase();
+
+        try {
+            PreparedStatement ps = ApiNetuno.getInstance().getConnectionManager()
+                    .getConn().prepareStatement( "UPDATE random SET value = ? WHERE key = ?;" );
+            ps.setString( 1, slow + "" );
+            ps.setString( 2, DATA_KEY );
+
+            ps.addBatch();
+            ps.executeBatch();
+            ps.close();
+        } catch ( SQLException e ) {
+            throw new RuntimeException( e );
+        }
     }
 }
