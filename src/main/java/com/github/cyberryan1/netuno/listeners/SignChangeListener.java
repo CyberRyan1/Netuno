38ec535c02f1dd26b322367f9b6cfe6837b1b0b5
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-04 03:44:42 -0500

    Refactored to use NetunoApi

--- src/main/java/com/github/cyberryan1/netuno/listeners/SignChangeListener.java
+++ src/main/java/com/github/cyberryan1/netuno/listeners/SignChangeListener.java
@@ -1,11 +1,15 @@
 package com.github.cyberryan1.netuno.listeners;
 
+import com.github.cyberryan1.cybercore.CyberCore;
 import com.github.cyberryan1.cybercore.utils.VaultUtils;
-import com.github.cyberryan1.netuno.classes.IPPunishment;
-import com.github.cyberryan1.netuno.classes.Punishment;
+import com.github.cyberryan1.netuno.api.database.SignNotifs;
+import com.github.cyberryan1.netuno.api.models.players.NetunoPlayer;
+import com.github.cyberryan1.netuno.api.models.players.NetunoPlayerCache;
 import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.database.Database;
+import com.github.cyberryan1.netuno.utils.settings.Settings;
 import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
+import com.github.cyberryan1.netunoapi.models.punishments.NPunishment;
+import com.github.cyberryan1.netunoapi.models.punishments.PunishmentType;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
@@ -13,11 +17,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SignChangeListener implements Listener {
-
-    private final Database DATA = Utils.getDatabase();
 
     @EventHandler
     public void onSignChange( SignChangeEvent event ) {
@@ -27,17 +30,20 @@ public class SignChangeListener implements Listener {
         if ( lines[0].equals( "" ) == false || lines[1].equals( "" ) == false || lines[2].equals( "" ) == false || lines[3].equals( "" ) == false ) {
             Player player = event.getPlayer();
 
-            if ( YMLUtils.getConfig().getBool( "signs.allow-while-muted" ) == false ) {
-                ArrayList<Punishment> puns = DATA.getPunishment( player.getUniqueId().toString(), "mute", true );
-                ArrayList<IPPunishment> ipPuns = DATA.getIPPunishment( player.getUniqueId().toString(), "ipmute", true );
-                if ( ipPuns.size() + puns.size() > 0 ) {
+            if ( Settings.ALLOW_SIGNS_WHILE_PUNISHED.bool() == false ) {
+                final NetunoPlayer nPlayer = NetunoPlayerCache.getOrLoad( player.getUniqueId().toString() );
+
+                List<NPunishment> puns = nPlayer.getPunishments().stream()
+                        .filter( pun -> pun.isActive() && ( pun.getPunishmentType() == PunishmentType.MUTE || pun.getPunishmentType() == PunishmentType.IPMUTE ) )
+                        .collect( Collectors.toList() );
+                if ( puns.size() > 0 ) {
                     event.setCancelled( true );
-                    player.sendMessage( YMLUtils.getConfig().getColoredStr( "signs.sign-while-muted-attempt" ) );
+                    player.sendMessage( Settings.SIGN_WHILE_PUNISHED_MESSAGE.coloredString() );
                     return;
                 }
             }
 
-            if ( YMLUtils.getConfig().getBool( "signs.notifs" ) ) {
+            if ( Settings.SIGN_NOTIFS_ENABLED.bool() ) {
                 String msg = Utils.getCombinedString( YMLUtils.getConfig().getColoredStrList( "signs.notifs-msg" ) );
 
                 if ( msg.contains( "[LINE_1]" ) == false || msg.contains( "[LINE_2]" ) == false
@@ -52,17 +58,19 @@ public class SignChangeListener implements Listener {
                 msg = msg.replace( "[LINE_3]", lines[2] ).replace( "[LINE_4]", lines[3] );
 
                 if ( msg.contains( "[LOC]" ) ) {
-                //    String loc = "%s, %s, %s".format( sign.getLocation().getX() + "", sign.getLocation().getY() + "", sign.getLocation().getZ() + "" );
                     String loc = sign.getX() + ", " + sign.getY() + ", " + sign.getZ();
                     msg = msg.replace( "[LOC]", loc );
                 }
 
+                final String finalMsg = msg;
                 for ( Player p : Bukkit.getOnlinePlayers() ) {
-                    if ( VaultUtils.hasPerms( p, YMLUtils.getConfig().getStr( "signs.notifs-perm" ) )
-                            && VaultUtils.hasPerms( p, YMLUtils.getConfig().getStr( "general.staff-perm" ) ) ) {
-                        if ( DATA.checkPlayerNoSignNotifs( p ) == false ) {
-                            Utils.sendAnyMsg( p, msg );
-                        }
+                    if ( VaultUtils.hasPerms( p, Settings.SIGN_NOTIFS_PERMISSION.string() )
+                            && VaultUtils.hasPerms( p, Settings.STAFF_PERMISSION.string() ) ) {
+                        Bukkit.getScheduler().runTaskAsynchronously( CyberCore.getPlugin(), () -> {
+                            if ( SignNotifs.playerHasNoSignNotifs( p.getUniqueId().toString() ) == false ) {
+                                Utils.sendAnyMsg( p, finalMsg );
+                            }
+                        } );
                     }
                 }
             }
