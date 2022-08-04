acfd54d5c1c06d3057820cc6e78e2b447e8d3b4c
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-03 21:19:59 -0500

    Added the SQLiteTables creation enum

---
+++ src/main/java/com/github/cyberryan1/netuno/api/database/helpers/SQLiteTables.java
@@ -0,0 +1,36 @@
+package com.github.cyberryan1.netuno.api.database.helpers;
+
+public enum SQLiteTables {
+
+    PUNS_TABLE( "CREATE TABLE IF NOT EXISTS punishments (" +
+            "`id` INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "`player` STRING NOT NULL," +
+            "`data` BLOB NOT NULL," +
+            "`guipun` STRING NOT NULL," +
+            "`reference` INTEGER NOT NULL );"
+    ),
+
+    ALTS_TABLE( "CREATE TABLE IF NOT EXISTS alts ("  +
+            "`group` INTEGER," +
+            "`item` STRING NOT NULL," +
+            "`type` STRING NOT NULL );"
+    ),
+
+    REPORTS_TABLE( "CREATE TABLE IF NOT EXISTS reports (" +
+            "`id` INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "`player` STRING NOT NULL," +
+            "`data` BLOB NOT NULL );"
+    ),
+
+    RANDOM_TABLE( "CREATE TABLE IF NOT EXISTS random (" +
+            "`key` STRING NOT NULL," +
+            "`value` STRING NOT NULL );"
+    );
+
+    private String sql;
+    SQLiteTables( String sql ) {
+        this.sql = sql;
+    }
+
+    public String getSql() { return sql; }
+}
