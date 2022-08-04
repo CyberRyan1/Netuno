5c418c3d2c5483e7d562a91cc68dafad86c964d0
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-04 03:44:53 -0500

    Added the SignNotifs class

---
+++ src/main/java/com/github/cyberryan1/netuno/api/database/SignNotifs.java
@@ -0,0 +1,50 @@
+package com.github.cyberryan1.netuno.api.database;
+
+import com.github.cyberryan1.netuno.api.ApiNetuno;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class SignNotifs {
+
+    public static void addPlayerNoSignNotifs( String playerUuid ) {
+        try {
+            PreparedStatement ps = ApiNetuno.getInstance().getConn().getConn().prepareStatement( "INSERT INTO random(key, value) VALUES(?, ?);" );
+            ps.setString( 1, "no-sign-notifs" );
+            ps.setString( 2, playerUuid );
+
+            ps.executeUpdate();
+            ps.close();
+        } catch ( SQLException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public static void removePlayerNoSignNotifs( String playerUuid ) {
+        try {
+            PreparedStatement ps = ApiNetuno.getInstance().getConn().getConn().prepareStatement( "DELETE FROM random WHERE key = ? AND value = ?;" );
+            ps.setString( 1, "no-sign-notifs" );
+            ps.setString( 2, playerUuid );
+
+            ps.executeUpdate();
+            ps.close();
+        } catch ( SQLException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public static boolean playerHasNoSignNotifs( String playerUuid ) {
+        boolean toReturn = false;
+        try {
+            PreparedStatement ps = ApiNetuno.getInstance().getConn().getConn().prepareStatement( "SELECT * FROM random WHERE key = ? AND value = ?;" );
+            ps.setString( 1, "no-sign-notifs" );
+            ps.setString( 2, playerUuid );
+
+            toReturn = ps.executeQuery().next();
+            ps.close();
+        } catch ( SQLException e ) {
+            throw new RuntimeException( e );
+        }
+        return toReturn;
+    }
+}
