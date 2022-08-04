eeb3e5f107670260cc2ef48f29670d4d7ff0deff
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-04 03:31:18 -0500

    Refactored to use NetunoApi

--- src/main/java/com/github/cyberryan1/netuno/listeners/CommandListener.java
+++ src/main/java/com/github/cyberryan1/netuno/listeners/CommandListener.java
@@ -1,8 +1,9 @@
 package com.github.cyberryan1.netuno.listeners;
 
-import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.database.Database;
+import com.github.cyberryan1.netuno.api.ApiNetuno;
 import com.github.cyberryan1.netuno.utils.yml.YMLUtils;
+import com.github.cyberryan1.netunoapi.models.punishments.NPunishment;
+import com.github.cyberryan1.netunoapi.models.punishments.PunishmentType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -12,12 +13,15 @@ import java.util.List;
 
 public class CommandListener implements Listener {
 
-    private final Database DATA = Utils.getDatabase();
-
     @EventHandler
     public void onPlayerCommand( PlayerCommandPreprocessEvent event ) {
-        if ( DATA.getPunishment( event.getPlayer().getUniqueId().toString(), "mute", true ).size() >= 1
-                || DATA.getIPPunishment( event.getPlayer().getUniqueId().toString(), "ipmute", true ).size() >= 1 ) {
+        List<NPunishment> punishments = ApiNetuno.getData().getPun().getPunishments( event.getPlayer() );
+        boolean anyActive = punishments.stream()
+                .anyMatch( pun -> pun.isActive()
+                        && ( pun.getPunishmentType() == PunishmentType.MUTE || pun.getPunishmentType() == PunishmentType.IPMUTE )
+                );
+
+        if ( anyActive ) {
             String blockedCmdsList[] = YMLUtils.getConfig().getStrList( "mute.blocked-cmds" );
             if ( blockedCmdsList != null && blockedCmdsList.length > 0 ) {
                 ArrayList<String> blockedCmds = new ArrayList<>( List.of( blockedCmdsList ) );
