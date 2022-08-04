623c7a7c2eec8bb9a0a1706c9d291544661ebe4c
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-03 21:20:24 -0500

    Created the NetunoRandomDatabase class

---
+++ src/main/java/com/github/cyberryan1/netuno/api/database/NetunoRandomDatabase.java
@@ -0,0 +1,7 @@
+package com.github.cyberryan1.netuno.api.database;
+
+import com.github.cyberryan1.netunoapi.database.RandomDatabase;
+
+public class NetunoRandomDatabase implements RandomDatabase {
+
+}
