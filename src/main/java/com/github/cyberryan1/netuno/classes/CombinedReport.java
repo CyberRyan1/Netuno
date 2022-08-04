b4e4d7dc323a124445e8f8062351266a044db6b2
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-04 17:22:16 -0500

    Updated to use NetunoApi

--- src/main/java/com/github/cyberryan1/netuno/classes/CombinedReport.java
+++ src/main/java/com/github/cyberryan1/netuno/classes/CombinedReport.java
@@ -1,71 +1,71 @@
 package com.github.cyberryan1.netuno.classes;
 
+import com.github.cyberryan1.cybercore.utils.CoreGUIUtils;
 import com.github.cyberryan1.cybercore.utils.CoreUtils;
+import com.github.cyberryan1.netuno.api.ApiNetuno;
 import com.github.cyberryan1.netuno.guis.utils.GUIUtils;
-import com.github.cyberryan1.netuno.utils.Time;
-import com.github.cyberryan1.netuno.utils.Utils;
-import com.github.cyberryan1.netuno.utils.database.Database;
+import com.github.cyberryan1.netunoapi.models.reports.NReport;
+import com.github.cyberryan1.netunoapi.models.time.NDate;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class CombinedReport {
 
-    private final Database DATA = Utils.getDatabase();
-
     private OfflinePlayer target;
-    private ArrayList<SingleReport> reports;
+    private List<NReport> reports;
     private long mostRecentDate;
 
     public CombinedReport( OfflinePlayer target ) {
         this.target = target;
 
-        reports = DATA.getReport( target.getUniqueId().toString() );
+        reports = ApiNetuno.getData().getNetunoReports().getReports( target );
         if ( reports.size() > 0 ) {
-            mostRecentDate = reports.get( 0 ).getDate();
-            for ( SingleReport r : reports ) {
-                if ( r.getDate() > mostRecentDate ) { mostRecentDate = r.getDate(); }
+            mostRecentDate = reports.get( 0 ).getTimestamp();
+            for ( NReport nr : reports ) {
+                if ( nr.getTimestamp() > mostRecentDate ) { mostRecentDate = nr.getTimestamp(); }
             }
         }
     }
 
     public OfflinePlayer getTarget() { return target; }
 
-    public ArrayList<SingleReport> getAllReports() { return reports; }
+    public List<NReport> getAllReports() { return reports; }
 
     public long getMostRecentDate() { return mostRecentDate; }
 
     public ItemStack getAsItem() {
         ItemStack skull = GUIUtils.getPlayerSkull( target );
 
-        skull = GUIUtils.setItemName( skull, "&p" + target.getName() );
+        skull = CoreGUIUtils.setItemName( skull, "&p" + target.getName() );
 
         //      Reason, Amount
         HashMap<String, Integer> reasonAmount = new HashMap<>();
-        for ( SingleReport r : reports ) {
-            if ( reasonAmount.containsKey( r.getReason() ) ) {
-                int amount = reasonAmount.get( r.getReason() ) + 1;
-                reasonAmount.replace( r.getReason(), amount );
+        for ( NReport nr : reports ) {
+            if ( reasonAmount.containsKey( nr.getReason() ) ) {
+                int amount = reasonAmount.get( nr.getReason() ) + 1;
+                reasonAmount.replace( nr.getReason(), amount );
             }
             else {
-                reasonAmount.put( r.getReason(), 1 );
+                reasonAmount.put( nr.getReason(), 1 );
             }
         }
 
-        ArrayList<String> reasons = new ArrayList<>();
+        List<String> reasons = new ArrayList<>();
         for ( String rea : reasonAmount.keySet() ) {
             int amount = reasonAmount.get( rea );
             reasons.add( CoreUtils.getColored( " &8- &p" + amount + "x &s" + rea ) );
         }
 
         ArrayList<String> lore = new ArrayList<>();
-        lore.add( CoreUtils.getColored( "&pDate: &s" + Time.getDateFromTimestamp( mostRecentDate ) ) );
+        lore.add( CoreUtils.getColored( "&pDate: &s" + new NDate( mostRecentDate ).getDateString() ) );
         lore.add( CoreUtils.getColored( "&pReason(s):" ) );
         lore.addAll( reasons );
 
-        return GUIUtils.setItemLore( skull, lore );
+        return CoreGUIUtils.setItemLore( skull, lore );
     }
 
 }
