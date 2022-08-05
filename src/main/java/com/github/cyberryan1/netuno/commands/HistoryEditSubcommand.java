a13ba76969198f5443bfb5a02d76ee800c0ba8ad
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-04 20:29:26 -0500

    Updated to use NetunoApi

--- src/main/java/com/github/cyberryan1/netuno/commands/HistoryEditSubcommand.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/HistoryEditSubcommand.java
@@ -4,18 +4,15 @@ import com.github.cyberryan1.cybercore.helpers.command.ArgType;
 import com.github.cyberryan1.cybercore.helpers.command.CyberSubcommand;
 import com.github.cyberryan1.cybercore.helpers.command.SubcommandStatus;
 import com.github.cyberryan1.cybercore.utils.VaultUtils;
-import com.github.cyberryan1.netuno.classes.Punishment;
+import com.github.cyberryan1.netuno.api.ApiNetuno;
 import com.github.cyberryan1.netuno.guis.history.NewHistoryEditGUI;
 import com.github.cyberryan1.netuno.utils.CommandErrors;
-import com.github.cyberryan1.netuno.utils.Utils;
 import com.github.cyberryan1.netuno.utils.settings.Settings;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
+import com.github.cyberryan1.netunoapi.models.punishments.NPunishment;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.List;
-import java.util.UUID;
 
 public class HistoryEditSubcommand extends CyberSubcommand {
 
@@ -44,12 +41,9 @@ public class HistoryEditSubcommand extends CyberSubcommand {
         final Player player = ( Player ) sender;
         final int punId = Integer.parseInt( args[1] );
 
-        Punishment pun = Utils.getDatabase().getPunishment( punId );
-        if ( pun == null ) { pun = Utils.getDatabase().getIPPunishment( punId ); }
+        NPunishment pun = ApiNetuno.getData().getNetunoPuns().getPunishment( punId );
         if ( pun != null ) {
-            final OfflinePlayer target = Bukkit.getOfflinePlayer( UUID.fromString( pun.getPlayerUUID() ) );
-
-            NewHistoryEditGUI gui = new NewHistoryEditGUI( target, player, punId );
+            NewHistoryEditGUI gui = new NewHistoryEditGUI( pun.getPlayer(), player, punId );
             gui.open();
         }
 
