47a419d2b54cd2ff73fcee27cf3ab772dda45472
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-04 20:29:18 -0500

    Updated to use NetunoApi

--- src/main/java/com/github/cyberryan1/netuno/commands/Kick.java
+++ src/main/java/com/github/cyberryan1/netuno/commands/Kick.java
@@ -2,10 +2,12 @@ package com.github.cyberryan1.netuno.commands;
 
 import com.github.cyberryan1.cybercore.helpers.command.ArgType;
 import com.github.cyberryan1.cybercore.helpers.command.CyberCommand;
-import com.github.cyberryan1.netuno.classes.PrePunishment;
+import com.github.cyberryan1.netuno.models.NetunoPrePunishment;
 import com.github.cyberryan1.netuno.utils.CommandErrors;
 import com.github.cyberryan1.netuno.utils.Utils;
 import com.github.cyberryan1.netuno.utils.settings.Settings;
+import com.github.cyberryan1.netunoapi.models.punishments.PunishmentType;
+import com.github.cyberryan1.netunoapi.utils.TimeUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
@@ -39,20 +41,21 @@ public class Kick extends CyberCommand {
     // /kick (player) (reason)
     public boolean execute( CommandSender sender, String args[] ) {
         final OfflinePlayer target = Bukkit.getOfflinePlayer( args[0] );
-        PrePunishment pun = new PrePunishment(
-                target,
-                "Kick",
-                Utils.getRemainingArgs( args, 1 )
-        );
 
-        pun.setConsoleSender( true );
+        NetunoPrePunishment pun = new NetunoPrePunishment();
+        pun.setPlayer( target );
+        pun.setPunishmentType( PunishmentType.KICK );
+        pun.setTimestamp( TimeUtils.getCurrentTimestamp() );
+        pun.setReason( Utils.getRemainingArgs( args, 1 ) );
+        pun.setLength( 0 );
+
+        pun.setStaffUuid( "CONSOLE" );
         if ( sender instanceof Player ) {
             Player staff = ( Player ) sender;
             pun.setStaff( staff );
-            pun.setConsoleSender( false );
 
             if ( Utils.checkStaffPunishmentAllowable( staff, target ) == false ) {
-                CommandErrors.sendPlayerCannotBePunished( sender, target.getName() );
+                CommandErrors.sendPlayerCannotBePunished( staff, target.getName() );
                 return true;
             }
         }
