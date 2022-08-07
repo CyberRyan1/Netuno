533b51c42e84ea16b6567f9c93381fcbf76300a1
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-07 13:58:51 -0500

    Added database.cache.reports.save-every setting

--- src/main/java/com/github/cyberryan1/netuno/utils/settings/Settings.java
+++ src/main/java/com/github/cyberryan1/netuno/utils/settings/Settings.java
@@ -184,6 +184,8 @@ public enum Settings {
     DATABASE_SQL_USERNAME( "database.sql.username", "string" ),
     DATABASE_SQL_PASSWORD( "database.sql.password", "string" ),
 
+
+    CACHE_REPORTS_SAVE_EVERY( "database.cache.reports.save-every", "int" ),
     ;
 
     private String path;
