d336eca195beba3adffb7ba2319ee4fdb90290fa
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-19 19:59:53 -0500

    Added the ymlName variable to the constructor

--- src/main/java/com/github/cyberryan1/netuno/guis/punish/utils/MultiPunishButton.java
+++ src/main/java/com/github/cyberryan1/netuno/guis/punish/utils/MultiPunishButton.java
@@ -9,14 +9,14 @@ public class MultiPunishButton {
 
     private List<SinglePunishButton> buttons = new ArrayList<>();
 
-    public MultiPunishButton( String guiPath ) {
+    public MultiPunishButton( String guiPath, String ymlName ) {
         this.guiPath = guiPath;
 
         for ( int index = 10; index <= 34; index++ ) {
             // Skipping the first, middle, and last columns in the GUI
             if ( index % 9 == 0 || index % 9 == 4 || index % 9 == 8 ) { continue; }
 
-            buttons.add( new SinglePunishButton( guiPath + "." + index ) );
+            buttons.add( new SinglePunishButton( guiPath + "." + index, ymlName ) );
         }
     }
 
