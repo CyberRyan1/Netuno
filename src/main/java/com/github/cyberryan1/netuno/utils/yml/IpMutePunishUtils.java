4843298ed6d58fe3be94c397c77e9d3fa0e8dda5
CyberRyan <cyberryan934@gmail.com>
2022-12-06 22:36:22 -0600

    Migrated all YML helper classes

--- src/main/java/com/github/cyberryan1/netuno/utils/yml/IpMutePunishUtils.java
+++ src/main/java/com/github/cyberryan1/netuno/utils/yml/IpMutePunishUtils.java
@@ -1,11 +1,11 @@
 package com.github.cyberryan1.netuno.utils.yml;
 
-import com.github.cyberryan1.cybercore.managers.YmlManager;
-import com.github.cyberryan1.cybercore.utils.yml.YMLReadTemplate;
+import com.github.cyberryan1.cybercore.spigot.config.YmlLoader;
+import com.github.cyberryan1.cybercore.spigot.config.YmlReader;
 
-public class IpMutePunishUtils extends YMLReadTemplate {
+public class IpMutePunishUtils extends YmlReader {
 
     public IpMutePunishUtils() {
-        super.setYMLManager( new YmlManager( "guis/ipmute_punish_gui.yml", "ipmute_punish_gui_default.yml" ) );
+        super( new YmlLoader(  "guis/ipmute_punish_gui.yml", "ipmute_punish_gui_default.yml" ) );
     }
 }
