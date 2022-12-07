08c94e781bb9e6d64cc07127a70062a81307376c
CyberRyan <cyberryan934@gmail.com>
2022-12-07 15:50:10 -0600

    Created the ActiveGuiManager manager class

---
+++ src/main/java/com/github/cyberryan1/netuno/guis/punish/managers/ActiveGuiManager.java
@@ -0,0 +1,59 @@
+package com.github.cyberryan1.netuno.guis.punish.managers;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ActiveGuiManager {
+
+    private static final List<OpenGui> activeGuis = new ArrayList<>();
+
+    public static void addActiveGui( Player staff, OfflinePlayer target ) {
+        activeGuis.add( new OpenGui( staff, target ) );
+    }
+
+    public static void attemptRemoveActiveGui( Player staff ) {
+        for ( OpenGui gui : activeGuis ) {
+            if ( gui.getStaff().getUniqueId().equals( staff.getUniqueId() ) ) {
+                if ( gui.isCancelDelete() ) {
+                    gui.setCancelDelete( false );
+                    return;
+                }
+                activeGuis.remove( gui );
+                return;
+            }
+        }
+    }
+
+    public static void forceRemoveActiveGui( Player staff ) {
+        for ( OpenGui gui : activeGuis ) {
+            if ( gui.getStaff().getUniqueId().equals( staff.getUniqueId() ) ) {
+                activeGuis.remove( gui );
+                return;
+            }
+        }
+    }
+
+    public static Optional<OpenGui> searchByStaff( Player staff ) {
+        for ( OpenGui gui : activeGuis ) {
+            if ( gui.getStaff().getUniqueId().equals( staff.getUniqueId() ) ) {
+                return Optional.of( gui );
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    public static Optional<OpenGui> searchByTarget( OfflinePlayer target ) {
+        for ( OpenGui gui : activeGuis ) {
+            if ( gui.getTarget().getUniqueId().equals( target.getUniqueId() ) ) {
+                return Optional.of( gui );
+            }
+        }
+
+        return Optional.empty();
+    }
+}
