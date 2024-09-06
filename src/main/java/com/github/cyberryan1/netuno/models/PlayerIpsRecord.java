aea14f09d2fe85c51697382b195bef1f66b7c7c9
Ryan <RyanCocks@my.unt.edu>
2024-09-05 21:53:52 -0400

    Testing

--- src/main/java/com/github/cyberryan1/netuno/models/PlayerIpsRecord.java
+++ src/main/java/com/github/cyberryan1/netuno/models/PlayerIpsRecord.java
@@ -12,7 +12,6 @@ import java.util.UUID;
  */
 public class PlayerIpsRecord /*implements ApiPlayerIpList*/ {
 
-    // test2
     private final UUID uuid;
     private final List<String> ipList;
     private boolean dataMatchesDatabase;
