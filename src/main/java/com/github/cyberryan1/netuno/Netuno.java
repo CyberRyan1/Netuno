489c15f31c7a389fb1883b60f8b36175ad333830
Ryan <ryan.cocks@mail.utoronto.ca>
2025-04-05 15:12:52 -0400

    Updating todo lists

--- src/main/java/com/github/cyberryan1/netuno/Netuno.java
+++ src/main/java/com/github/cyberryan1/netuno/Netuno.java
@@ -93,8 +93,6 @@ Config:
 - Change config updater to better one (do research) <-- want to use ConfigUpdater
  */
 
-// TODO punishment GUIs take time to load and don't load at the same time- try and synchronize them
-
 public final class Netuno extends JavaPlugin {
 
     // Database Connection
