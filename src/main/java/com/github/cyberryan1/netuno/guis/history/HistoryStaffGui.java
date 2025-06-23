9bbe5156d52124765362a9b5ac9bd35407dbdc4b
Ryan <ryan.cocks@mail.utoronto.ca>
2025-06-23 07:58:43 -0500

    WIP HistoryStaffGui

---
+++ src/main/java/com/github/cyberryan1/netuno/guis/history/HistoryStaffGui.java
@@ -0,0 +1,11 @@
+package com.github.cyberryan1.netuno.guis.history;
+
+/**
+ * Lists all punishments that have been executed by a player
+ * to the provided staff member
+ *
+ * @author Ryan
+ */
+public class HistoryStaffGui {
+    // TODO
+}
