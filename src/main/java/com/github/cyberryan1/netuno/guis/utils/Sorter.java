e593bf19dfc65746dd787fa4c424b5211265439e
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2025-07-04 20:44:18 -0500

    Added sortReports() method

--- src/main/java/com/github/cyberryan1/netuno/guis/utils/Sorter.java
+++ src/main/java/com/github/cyberryan1/netuno/guis/utils/Sorter.java
@@ -1,6 +1,9 @@
 package com.github.cyberryan1.netuno.guis.utils;
 
 import com.github.cyberryan1.netuno.api.models.ApiPunishment;
+import com.github.cyberryan1.netuno.api.models.ApiReport;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 
 import java.util.List;
 
@@ -48,5 +51,55 @@ public class Sorter {
                 break;
         }
         return puns;
+    }
+
+    /**
+     * Sorts a report list according to a provided sort order
+     *
+     * @param reports The report list to sort
+     * @param sort The sort order
+     * @return The sorted report list
+     */
+    public static List<ApiReport> sortReports( List<ApiReport> reports, SortBy sort ) {
+        if ( reports.size() <= 1 ) { return reports; }
+
+        switch ( sort ) {
+            case FIRST_DATE:
+                reports.sort( ( a, b ) -> ( int ) ( a.getReportDate() - b.getReportDate() ) );
+                break;
+            case LAST_DATE:
+                reports.sort( ( a, b ) -> ( int ) ( b.getReportDate() - a.getReportDate() ) );
+                break;
+            case ONLINE:
+                reports.sort( ( a, b ) -> {
+                    OfflinePlayer aPlayer = Bukkit.getOfflinePlayer( a.getPlayer() );
+                    OfflinePlayer bPlayer = Bukkit.getOfflinePlayer( b.getPlayer() );
+
+                    if ( aPlayer.isOnline() == bPlayer.isOnline() ) {
+                        return aPlayer.getName().compareToIgnoreCase( bPlayer.getName() );
+                    }
+                    return aPlayer.isOnline() ? -1 : 1;
+                } );
+                break;
+            case OFFLINE:
+                reports.sort( ( a, b ) -> {
+                    OfflinePlayer aPlayer = Bukkit.getOfflinePlayer( a.getPlayer() );
+                    OfflinePlayer bPlayer = Bukkit.getOfflinePlayer( b.getPlayer() );
+
+                    if ( aPlayer.isOnline() == bPlayer.isOnline() ) {
+                        return aPlayer.getName().compareToIgnoreCase( bPlayer.getName() );
+                    }
+                    return aPlayer.isOnline() ? 1 : -1;
+                } );
+                break;
+            case ALPHABETICAL:
+                reports.sort( ( a, b ) -> {
+                    String aName = Bukkit.getOfflinePlayer( a.getPlayer() ).getName();
+                    String bName = Bukkit.getOfflinePlayer( b.getPlayer() ).getName();
+                    return aName.compareToIgnoreCase( bName );
+                } );
+                break;
+        }
+        return reports;
     }
 }
