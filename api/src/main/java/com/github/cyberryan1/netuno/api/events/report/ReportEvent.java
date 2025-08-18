7f27fa52363d48586d78c06a088193d0fa5d0268
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2025-08-18 16:04:44 -0500

    Fixed issue with ReportEvent

--- api/src/main/java/com/github/cyberryan1/netuno/api/events/report/ReportEvent.java
+++ api/src/main/java/com/github/cyberryan1/netuno/api/events/report/ReportEvent.java
@@ -3,8 +3,6 @@ package com.github.cyberryan1.netuno.api.events.report;
 import com.github.cyberryan1.netuno.api.events.NetunoEvent;
 import com.github.cyberryan1.netuno.api.models.ApiReport;
 
-import java.util.List;
-
 /**
  * This event is fired when a player is reported
  *
@@ -12,16 +10,16 @@ import java.util.List;
  */
 public class ReportEvent implements NetunoEvent {
 
-    private final List<ApiReport> reports;
+    private final ApiReport report;
 
-    public ReportEvent( List<ApiReport> report ) {
-        this.reports = report;
+    public ReportEvent( ApiReport report ) {
+        this.report = report;
     }
 
     /**
-     * @return The list of {@link ApiReport} that were sent
+     * @return The {@link ApiReport} that was sent
      */
-    public List<ApiReport> getReport() {
-        return reports;
+    public ApiReport getReport() {
+        return report;
     }
 }
