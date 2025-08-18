4fe6e412e3dbb35101f7c50eb059ef1b39195344
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2025-08-18 15:01:15 -0500

    Moved api from main to api submodule

--- main/src/main/java/com/github/cyberryan1/netuno/api/package-info.java
+++
@@ -1,6 +0,0 @@
-package com.github.cyberryan1.netuno.api;
-
-/*
-Note: this package will be moved to NetunoAPI eventually, but for
-development purposes it will stay here in the main Netuno project
- */
